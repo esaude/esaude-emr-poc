020065567956ee9a2e6f9d628f583b8010aad0d8
valvijo <valeriovjoao@gmail.com>
2015-08-21 11:13:41 +0200

    verifying authentication before loading page

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
         ],
       livereload: {
         options: {
-          open: true,
+          open: "http://localhost:9000/home/",
           middleware: function (connect) {
             return [
               serveStatic('.tmp'),
--- app/home/app.js
+++ app/home/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular
-    .module('home', ['ngRoute', 'ngCookies', 'ui.router', 'application', 'chart.js', 'uiHelper'])
-    .config(['$urlRouterProvider', '$stateProvider', '$httpProvider', function ($urlRouterProvider, $stateProvider) {
+    .module('home', ['ngRoute', 'ngCookies', 'ui.router', 'application', 'chart.js', 'uiHelper', 'authentication'])
+    .config(['$urlRouterProvider', '$stateProvider', '$httpProvider', 
+                function ($urlRouterProvider, $stateProvider) {
         $urlRouterProvider.otherwise('/login');
         $stateProvider
             .state('login', {
@@ -17,7 +18,28 @@ angular
                 views: {
                     'layout': { templateUrl: '../common/application/views/layout.html', controller: 'DashboardController'},
                     'content@dashboard': { templateUrl: 'views/dashboard.html'}
-                }
+                },
+                resolve: {authenticate: authenticate}
             });
+            function authenticate($q, $cookieStore, $state, $timeout) {
+                console.log($cookieStore.get('user'));
+                if ($cookieStore.get('user') != null) {
+                    debugger;
+                    // Resolve the promise successfully
+                    return $q.when();
+                } else {
+                    debugger;
+                    // The next bit of code is asynchronously tricky.
+
+                    $timeout(function () {
+                        // This code runs after the authentication promise has been rejected.
+                        // Go to the log-in page
+                        $state.go('login', { showLoginMessage: true});
+                    });
+
+                    // Reject the authentication promise to prevent the state from loading
+                    return $q.reject();
+                }
+            }
             
     }]);
