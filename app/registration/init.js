06113057ca9906109a1a50c51ede093581a91250
valvijo <valeriovjoao@gmail.com>
2015-10-01 14:04:29 +0200

    using bootstrap switch for input of single selection

--- app/registration/init.js
+++ app/registration/init.js
@@ -1,2 +1,3 @@
 angular.module('registration', ['ngRoute', 'ngCookies', 'ui.router', 'smart-table', 'uiHelper', 'bahmni.common.config', 
-                        'bahmni.common.domain', 'bahmni.common.appFramework', 'LocalStorageModule', 'datePicker']);
+                        'bahmni.common.domain', 'bahmni.common.appFramework', 'LocalStorageModule', 'datePicker', 
+                        'frapontillo.bootstrap-switch']);
