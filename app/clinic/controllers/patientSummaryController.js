11fd32d575702dafc0d997167ba1f28ed1c8f402
vjoao <vjoao@pedaids.org>
2016-03-11 13:53:42 +0200

    displaying lab results on patient summary

--- app/clinic/controllers/patientSummaryController.js
+++ app/clinic/controllers/patientSummaryController.js
@@ -7,6 +7,9 @@ angular.module('clinic')
 
         (function () {
             patientUuid = $stateParams.patientUuid;
+        })();
+        
+        $scope.initVisitHistory = function () {
             encounterService.getEncountersOfPatient(patientUuid).success(function (data) {
                 var nonRetired = encounterService.filterRetiredEncoounters(data.results);
                 var grouped = _.groupBy(nonRetired, function (element) {
@@ -14,8 +17,25 @@ angular.module('clinic')
                 });
                 var reversed = _.values(grouped).reverse();
                 $scope.visits = reversed;
+            });
+        };
+        
+        $scope.initLabResults = function () {
+            var labEncounterUuid = "e2790f68-1d5f-11e0-b929-000c29ad1d07";//TODO: create in configuration file
+            
+            encounterService.getEncountersForEncounterType(patientUuid, labEncounterUuid).success(function (data) {
+                var nonRetired = encounterService.filterRetiredEncoounters(data.results);
+                var grouped = _.groupBy(nonRetired, function (element) {
+                    return Bahmni.Common.Util.DateUtil.getDate(element.encounterDatetime);
+                });
+                var reversed = _.values(grouped).reverse();
+                $scope.labs = reversed;
                 console.log(reversed);
             });
-        })();
+        };
+        
+        $scope.isObject = function (value) {
+            return _.isObject(value);
+        }
 
     }]);
