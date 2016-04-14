6cc6b9c22a78e622a8a61b09a6577ca279b2e29a
valvijo <valeriovjoao@gmail.com>
2016-04-14 15:41:39 +0200

    run before and after app

---
+++ app/registration/run-before-app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular
+    .module('registration')
+    .run(['$rootScope', '$state', '$stateParams',
+        function($rootScope, $state, $stateParams) {
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
+    ]);
