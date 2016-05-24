25f6d2e0c55075ff70aa3d2737486ab08206beda
Stélio Moiane <steliomo@gmail.com>
2016-05-24 10:50:58 +0200

    print patient demographics

--- app/patient-details/controllers/patientDetailController.js
+++ app/patient-details/controllers/patientDetailController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('patient.details')
-        .controller('DetailPatientController', ["$scope", "$stateParams", "$location",
-                function ($scope, $stateParams, $location) {
+        .controller('DetailPatientController', ["$scope", "$stateParams", "$location", "$http", "$rootScope", "$compile", "$timeout", "$q",
+                function($scope, $stateParams, $location, $http, $rootScope, $compile, $timeout, $q) {
             var patientUuid;
     
             (function () {
@@ -21,4 +21,54 @@ angular.module('patient.details')
             $scope.linkDashboard = function() {
                 $location.url("/dashboard/" + patientUuid); // path not hash
             };
+                    
+            // print the demographics info
+            $scope.print = function(){
+                
+                var templateUrl = "../patient-details/views/patient-demo-info.html";
+                var data = $scope.patient;
+                
+                //you need to transform this piece of code in a service
+                var printHtml = function (html) {
+                    var deferred = $q.defer();
+                    var hiddenFrame = $('<iframe style="display: none"></iframe>').appendTo('body')[0];
+
+                    hiddenFrame.contentWindow.printAndRemove = function() {
+                        hiddenFrame.contentWindow.print();
+                        $(hiddenFrame).remove();
+                    };
+
+                    var htmlContent = "<!doctype html>"+
+                                "<html>"+
+                                    '<body onload="printAndRemove();">' +
+                                        html +
+                                    '</body>'+
+                                "</html>";
+
+                    var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
+
+                    doc.write(htmlContent);
+                    deferred.resolve();
+                    doc.close();
+
+                    return deferred.promise;
+                };
+        
+                $http.get(templateUrl).success(function(template){
+
+                    var printScope = angular.extend($rootScope.$new(), data);
+                    var element = $compile($('<div>' + template + '</div>'))(printScope);
+
+                    var waitForRenderAndPrint = function() {
+                        if(printScope.$$phase || $http.pendingRequests.length) {
+                            $timeout(waitForRenderAndPrint);
+                        } else {
+                            printHtml(element.html());
+                            printScope.$destroy(); // To avoid memory leaks from scope create by $rootScope.$new()
+                        }
+                    };
+
+                    waitForRenderAndPrint();
+                });
+        }
         }]);
