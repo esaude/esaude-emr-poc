a94e5f6e83229849bc06c107e54cb4ebdd14bc03
valvijo <valeriovjoao@gmail.com>
2016-06-29 13:33:42 +0200

    created more form logic

--- app/poc-common/form-display/directives/formField.js
+++ app/poc-common/form-display/directives/formField.js
@@ -13,13 +13,54 @@ angular.module('poc.common.formdisplay')
             templateUrl: ' ../poc-common/form-display/views/formField.html',
             controller: 'FormFieldDirectiveController',
             scope: {
+                field: '=',
                 formParts: '=',
                 fieldUuid: '=',
                 fieldId: '='
             }
         };
     })
-    .controller('FormFieldDirectiveController', function ($scope) {
+    .controller('FormFieldDirectiveController', ['$rootScope', '$scope', 'observationsService', function ($rootScope, 
+        $scope, observationsService) {
+            
+        var formLogic = {};
+        
+        formLogic.defaultValueIsLastEntry = function (param) {
+            observationsService.get($rootScope.patient.uuid, param).success(function (data) {
+                var nonRetired = observationsService.filterRetiredObs(data.results);
+                if (!_.isEmpty(nonRetired)) {
+                    var last = _.maxBy(nonRetired, 'obsDatetime');
+                    $scope.fieldModel.value = last.value;
+                }
+            });
+        };
+        
+        formLogic.calculateOnChange = function (param) {
+            $scope.$watch('fieldModel.value', function (value) {
+                alert(value);
+            });
+        };
+        
+        formLogic.calculateWithLastObs = function (param) {
+            var tokens = _.split(param, ',');
+            var formula = "";
+            //find uuid and replace by value
+            _.forEach(tokens, function (token) {
+                if(validator.isUUID(token)) {
+                    observationsService.get($rootScope.patient.uuid, token).success(function (data) {
+                        var nonRetired = observationsService.filterRetiredObs(data.results);
+                        if (!_.isEmpty(nonRetired)) {
+                            var last = _.maxBy(nonRetired, 'obsDatetime');
+                            token = last.value;
+                            debugger
+                        }
+                    });
+                }
+                formula += token;
+            });
+            $scope.fieldModel.value = eval(formula);
+        };
+            
         (function () {
             $scope.$watch('$parent.submitted', function (value) {
                 $scope.showMessages = value;
@@ -34,5 +75,18 @@ angular.module('poc.common.formdisplay')
                 }
             });
             
+            $scope.initFieldModel = function () {
+                $scope.fieldModel = $scope.formParts.form.fields[$scope.fieldUuid];
+                if(!$scope.fieldModel.value) {
+                    _.forEach($scope.field.logics, function (param, name) {
+                        formLogic[name](param);
+                    });
+                }
+            };
+            
         })();
-    });
+        
+        var compactName = function (name) {
+            return name.trim().replace(/[^a-zA-Z0-9]/g, '');
+        };
+    }]);
