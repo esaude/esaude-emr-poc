2752f5466e9bdd9e960511b27bd1756b37d02e7a
Pascal Brandt <psbrandt@gmail.com>
2016-08-24 16:44:04 +0200

    🏹 Deploy only to test on tag.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -o xtrace
 
-# Deploy to dev
-echo "Deploying to dev."
-sshpass -p$TRAVIS_USER_PASSWORD ssh -o StrictHostKeyChecking=no travis@dev.esaude.org 'curl -sL https://raw.githubusercontent.com/esaude/esaude-poc-docker/master/scripts/dev-server-deploy.sh | bash';
-
-# Deploy to test if tag
 if [ -z "$TRAVIS_TAG" ];
 then
-  echo "Not deploying to test because this is not a tag.";
+  # Deploy to dev
+ echo "Deploying to dev."
+ sshpass -p$TRAVIS_USER_PASSWORD ssh -o StrictHostKeyChecking=no travis@dev.esaude.org 'curl -sL https://raw.githubusercontent.com/esaude/esaude-poc-docker/master/scripts/dev-server-deploy.sh | bash';
 else
+  # Deploy to test if tag
   echo "Deploying to test because this is a tag."
   sshpass -p$TRAVIS_TEST_USER_PASSWORD ssh -o StrictHostKeyChecking=no travis@test-ssh.esaude.org 'curl -sL https://raw.githubusercontent.com/esaude/esaude-poc-docker/master/scripts/dev-server-deploy.sh | POC_VERSION='"$TRAVIS_TAG"' bash';
 fi
