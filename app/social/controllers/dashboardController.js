69338a8adb742c371cfd87a20bf58c6292be189c
valvijo <valeriovjoao@gmail.com>
2016-08-29 13:56:24 +0200

    created module for social info

---
+++ app/social/controllers/dashboardController.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('social')
+        .controller('DashboardController', ["$rootScope", "$scope", "$location", "$stateParams", "patientService", "openmrsPatientMapper", function ($rootScope, $scope, $location, $stateParams, patientService, patientMapper) {
+            var patientUuid;
+    
+            init();
+    
+            function init() {
+                patientUuid = $stateParams.patientUuid;
+            
+                patientService.get(patientUuid).success(function (data) {
+                    $rootScope.patient = patientMapper.map(data);
+                });
+            }
+            
+            $scope.linkSearch = function() {
+                $location.url("/search"); // path not hash
+            };
+            
+            $scope.linkPatientDetail = function() {
+                $location.url("/patient/detail/" + patientUuid + "/demographic"); // path not hash
+            }; 
+        }]);
