faa2fead70219c0290b64adc97969e2f19e7654e
larslemos <larslemos@gmail.com>
2017-01-18 08:36:07 -0800

    Enhacement  #107 #120

--- app/registration/controllers/createPatientController.js
+++ app/registration/controllers/createPatientController.js
@@ -2,9 +2,9 @@
 
 angular.module('registration')
     .controller('CreatePatientController', ['$rootScope', '$scope', '$location', 'patient', 'patientService',
-                    'appService', 'openmrsPatientMapper',
-        function ($rootScope, $scope, $location, patientModel, patientService, appService, patientMapper) {
-                
+                    'appService', 'openmrsPatientMapper', 'notifier',
+        function ($rootScope, $scope, $location, patientModel, patientService, appService, patientMapper, notifier) {
+
                 $scope.actions = {};
                 $scope.addressHierarchyConfigs = appService.getAppDescriptor().getConfigValue("addressHierarchy");
 
@@ -12,7 +12,7 @@ angular.module('registration')
                     $scope.srefPrefix = "newpatient.";
                     $scope.patient = patientModel.create();
                 })();
-                
+
                 $scope.initAttributes = function() {
                     $scope.patientAttributes = [];
                     angular.forEach($scope.patientConfiguration.customAttributeRows(), function (value) {
@@ -21,7 +21,7 @@ angular.module('registration')
                         });
                     });
                 };
-                
+
                 $scope.save = function () {
                     var errMsg = Bahmni.Common.Util.ValidationUtil.validate($scope.patient,$scope.patientConfiguration.personAttributeTypes);
                     if(errMsg){
@@ -31,12 +31,13 @@ angular.module('registration')
 
                     patientService.create($scope.patient).success(successCallback);
                 };
-                
+
                 var successCallback = function (patientProfileData) {
                     $rootScope.patient = patientMapper.map(patientProfileData.patient);
                     $scope.patient.uuid = patientProfileData.patient.uuid;
                     $scope.patient.name = patientProfileData.patient.person.names[0].display;
                     $scope.patient.isNew = true;
+                    notifier.success("Patient Created");
                     $location.url("/dashboard/" + $scope.patient.uuid);
                 };
         }]);
