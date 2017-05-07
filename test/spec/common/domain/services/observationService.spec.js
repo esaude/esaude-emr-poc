ffa071709ede74b5ab0e6be0241f90f4849a2e52
valvijo <valeriovjoao@gmail.com>
2017-05-07 20:43:59 +0200

    fixed unit tests

--- test/spec/common/domain/services/observationService.spec.js
+++ test/spec/common/domain/services/observationService.spec.js
@@ -11,20 +11,4 @@ describe("observationsService", function () {
     });
   });
 
-  describe("fetchForPatient", function () {
-    it("should fetch observations for encounter", function () {
-      mockBackend
-        .expectGET('/openmrs/ws/rest/v1/bahmnicore/observations?concept=concept+name&patientUuid=encounterUuid')
-        .respond({results: ["Some data"]});
-
-      observationsService.fetch("encounterUuid", ["concept name"]).then(function (response) {
-        expect(response.data.results.length).toBe(1);
-        expect(response.data.results[0]).toBe("Some data");
-
-      });
-
-      mockBackend.flush();
-    })
-  });
-
 });
