79a14bc23efa6f9839797e17c1f05105525f726b
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-05-30 12:48:49 +0200

    Move print logic to reportService.

--- app/patient-details/controllers/patientDetailController.js
+++ app/patient-details/controllers/patientDetailController.js
@@ -4,9 +4,9 @@
   angular.module('patient.details')
     .controller('DetailPatientController', DetailPatientController);
 
-  DetailPatientController.$inject = ["$scope", "$stateParams", "$location", "$http", "$rootScope", "$compile", "$timeout", "$q"];
+  DetailPatientController.$inject = ["$scope", "$stateParams", "$location", "$http", "$rootScope", "$compile", "$timeout", "$q", "reportService"];
 
-  function DetailPatientController($scope, $stateParams, $location, $http, $rootScope, $compile, $timeout, $q) {
+  function DetailPatientController($scope, $stateParams, $location, reportService) {
     var vm = this;
 
     vm.patient = $scope.patient;
@@ -28,66 +28,8 @@
       $location.url("/dashboard/" + $stateParams.patientUuid); // path not hash
     }
 
-    // print the demographics info
     function print() {
-
-      var templateUrl = "../patient-details/views/patient-demo-info.html";
-      var data = vm.patient;
-      vm.patient.barcodeOptions = {
-        width: 2,
-        height: 40,
-        quite: 10,
-        displayValue: true,
-        font: "monospace",
-        textAlign: "center",
-        fontSize: 10,
-        backgroundColor: "",
-        lineColor: "#000"
-      };
-
-
-      //you need to transform this piece of code in a service
-      var printHtml = function (html) {
-        var deferred = $q.defer();
-        var hiddenFrame = $('<iframe style="display: none"></iframe>').appendTo('body')[0];
-
-        hiddenFrame.contentWindow.printAndRemove = function () {
-          hiddenFrame.contentWindow.print();
-          $(hiddenFrame).remove();
-        };
-
-        var htmlContent = "<!doctype html>" +
-          "<html>" +
-          '<body onload="printAndRemove();">' +
-          html +
-          '</body>' +
-          "</html>";
-
-        var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
-
-        doc.write(htmlContent);
-        deferred.resolve();
-        doc.close();
-
-        return deferred.promise;
-      };
-
-      $http.get(templateUrl).success(function (template) {
-
-        var printScope = angular.extend($rootScope.$new(), data);
-        var element = $compile($('<div>' + template + '</div>'))(printScope);
-
-        var waitForRenderAndPrint = function () {
-          if (printScope.$$phase || $http.pendingRequests.length) {
-            $timeout(waitForRenderAndPrint);
-          } else {
-            printHtml(element.html());
-            printScope.$destroy(); // To avoid memory leaks from scope create by $rootScope.$new()
-          }
-        };
-
-        waitForRenderAndPrint();
-      });
+      reportService.printPatientARVPickupHistoryReport(vm.patient);
     }
   }
 })();
---
+++ app/patient-details/services/reportService.js
@@ -0,0 +1,98 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('patient.details')
+    .factory('reportService', reportService);
+
+  reportService.$inject = ["$rootScope", "$compile", "$timeout", "$http", "$log", "$q"];
+
+  function reportService($rootScope, $compile, $timeout, $http, $log, $q) {
+
+    var PATIENT_ARV_PICKUP_HISTORY_REPORT_TEMPLATE = "../patient-details/views/patient-arv-pickup-history-report.html";
+
+    return {
+      printPatientARVPickupHistoryReport: printPatientARVPickupHistoryReport
+    };
+
+    ////////////////
+
+    /**
+     * @param {Object} patient
+     */
+    function printPatientARVPickupHistoryReport(patient) {
+      patient.barcodeOptions = {
+        width: 2,
+        height: 40,
+        quite: 10,
+        displayValue: true,
+        font: "monospace",
+        textAlign: "center",
+        fontSize: 10,
+        backgroundColor: "",
+        lineColor: "#000"
+      };
+
+      loadTemplate(PATIENT_ARV_PICKUP_HISTORY_REPORT_TEMPLATE)
+        .then(compile(patient))
+        .then(printHTML);
+    }
+
+    /**
+     * Generates a hidden iframe containing the html then prints and removes the iframe.
+     *
+     * @param {String} html
+     */
+    function printHTML(html) {
+      var hiddenFrame = angular.element('<iframe>').css('display', 'none').appendTo('body')[0];
+
+      hiddenFrame.contentWindow.printAndRemove = function () {
+        hiddenFrame.contentWindow.print();
+        angular.element(hiddenFrame).remove();
+      };
+
+      var htmlContent = "<!doctype html>" +
+        "<html>" +
+          '<body onload="printAndRemove();">' +
+            html +
+          '</body>' +
+        "</html>";
+
+      var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
+      doc.write(htmlContent);
+      doc.close();
+    }
+
+    /**
+     * @param {String} templateUrl
+     * @returns {Promise}
+     */
+    function loadTemplate(templateUrl) {
+      return $http.get(templateUrl)
+        .then(function (response) {
+          return response.data;
+        })
+        .catch(function (error) {
+          $log.error('XHR Failed for getPatientPharmacyEncounters. ' + error.data);
+          return $q.reject(error);
+        });
+    }
+
+    /**
+     * @param {Object} data
+     * @returns {Function} Function returning a promise that will be resolved when the template finishes compiling.
+     */
+    function compile(data) {
+      return function (template) {
+        var printScope = angular.extend($rootScope.$new(false), data);
+        var element = $compile(angular.element('<div>' + template + '</div>'))(printScope);
+
+        return $timeout(function () {
+          printScope.$destroy();
+          return element.html();
+        }, 0, false);
+      }
+    }
+  }
+
+})();
