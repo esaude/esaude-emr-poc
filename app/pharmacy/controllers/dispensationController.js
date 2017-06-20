0a35f681c1e88f7a022b855aac76c1915c1ea781
Guimino <minoneves@gmail.com>
2017-06-20 08:49:13 +0200

    Dispensation feature using Drugs

--- app/pharmacy/controllers/dispensationController.js
+++ app/pharmacy/controllers/dispensationController.js
@@ -16,34 +16,66 @@ function DispensationController($scope, $rootScope, $filter, dispensationService
 
         $scope.today = dateUtil.getDateWithoutTime(dateUtil.now());
 
-        var arvConcepUuid = "e1d83d4a-1d5f-11e0-b929-000c29ad1d07";
-
         $scope.prescriptiontNoResultsMessage = "PHARMACY_LIST_NO_ITEMS";
 
         $scope.initPrescriptions = function () {
 
             var patientUuid = $rootScope.patient.uuid;
 
-            prescriptionService.getPatientPrescriptions(patientUuid).then(function (prescriptions) {
-                $scope.prescriptions = prescriptions;
-                $scope.prescription = prescriptions[0];
-                $scope.prescriptiontNoResultsMessage = _.isEmpty($scope.prescriptions) ? "PHARMACY_LIST_NO_ITEMS" : null;
-            });
+           prescriptionService.getPatientPrescriptions(patientUuid).then(function (prescriptions) {
+                           $scope.prescriptions = prescriptions;
+                           $scope.prescription = prescriptions[0];
+                           $scope.prescriptiontNoResultsMessage = _.isEmpty($scope.prescriptions) ? "PHARMACY_LIST_NO_ITEMS" : null;
+               });
         };
 
         $scope.select = function (item) {
-            item.disable = true;
-            if (arvConcepUuid === item.conceptParentUuid) {
-                item.showNextPickupDate = true;
-                item.quantity = 1;
-                item.nextPickupDate = new Date();
-            }
 
-            $scope.selectedItems.push(item);
-            $scope.updateDispenseListMessage();
+           if(!item.disable){
+
+               $scope.setSelectedItem(item);
+               $scope.setDefaultArvData(item);
+               $scope.selectOtherDrugWithSomeRegime(item);
+            }
+        };
+
+        $scope.setSelectedItem = function(item){
+
+           item.disable = true;
+           $scope.selectedItems.push(item);
+           $scope.updateDispenseListMessage();
+        };
+
+        $scope.setDefaultArvData = function(item){
+
+          if (item.drugRegime) {
+
+              item.showNextPickupDate = true;
+              item.quantity = 1;
+              item.nextPickupDate = new Date();
+          }
+        };
+
+        $scope.selectOtherDrugWithSomeRegime = function(item){
+
+          _.forEach($scope.prescriptions, function (selectedItem) {
+
+              if(!selectedItem.disable)
+              {
+                  if( item.drugRegime && selectedItem.drugRegime){
+
+                      if(item.drugRegime.regime.uuid == selectedItem.drugRegime.regime.uuid){
+                          $scope.setSelectedItem(selectedItem);
+                          $scope.setDefaultArvData(selectedItem);
+                      }
+                  }
+              }
+          });
         };
 
         //fake just for now
+
+        /* TODO: Should Remove the hardedCode
         $scope.barcodeHandler = function(code) {
             // fake dcode
             var invalidDrug = 'IZONIAZID100mg';
@@ -64,15 +96,20 @@ function DispensationController($scope, $rootScope, $filter, dispensationService
                 $scope.itemIndex++;
             });
         };
-
+*/
         $scope.remove = function (item) {
+
+           $scope.setRemovedItem(item);
+           $scope.removeSimilarArvRegimeDrug(item);
+
+        };
+
+        $scope.setRemovedItem = function(item){
             item.disable = false;
             _.pull($scope.selectedItems, item);
-
             item.quantity = null;
             item.nextPickupDate = null;
-
-           $scope.updateDispenseListMessage();
+            $scope.updateDispenseListMessage();
         };
 
          $scope.updateDispenseListMessage = function () {
@@ -83,34 +120,90 @@ function DispensationController($scope, $rootScope, $filter, dispensationService
 
     })();
 
+    $scope.removeSimilarArvRegimeDrug = function(item){
+
+       _.forEach($scope.selectedItems, function (selectedItem) {
+
+                    if(selectedItem.disable)
+                    {
+                        if( item.drugRegime && selectedItem.drugRegime){
+
+                            if(item.drugRegime.regime.uuid == selectedItem.drugRegime.regime.uuid){
+
+                                $scope.setRemovedItem(selectedItem);
+                            }
+                        }
+                    }
+                });
+
+    };
+
     $scope.updatePickUp = function (item) {
 
-        if(item.quantity > item.drugToPickUp){
-            item.quantity = item.drugToPickUp;
-        }
+        $scope.setUpdatePickUp(item);
+        $scope.setUpdatePickUpForSimilarDrugRegime(item);
+    };
 
-        var twoDays = 2;
-        var sunday = 0;
-        var saturday = 6;
+    $scope.setUpdatePickUp = function(item){
 
-        var today = new Date();
-        var numberOfPillsMinusTwoDays = item.quantity;
-        var oneDayInMilSec = 1000 * 60 * 60 * 24;
+          if(item.quantity > item.drugToPickUp){
+                      item.quantity = item.drugToPickUp;
+          }
 
-        if(!item.quantity){
-            item.nextPickupDate = today;
-            return;
-        }
+          var twoDays = 2;
+          var sunday = 0;
+          var saturday = 6;
 
-        if(item.quantity >= twoDays){
-            numberOfPillsMinusTwoDays -= twoDays;
-        }
+          var today = new Date();
+          var numberOfPillsMinusTwoDays = item.quantity;
+          var oneDayInMilSec = 1000 * 60 * 60 * 24;
 
-        item.nextPickupDate = new Date(today.getTime() + (oneDayInMilSec * numberOfPillsMinusTwoDays));
+          if(!item.quantity){
+              item.nextPickupDate = today;
+              return;
+          }
 
-        while(item.nextPickupDate.getDay() == sunday || item.nextPickupDate.getDay() == saturday){
-            item.nextPickupDate = new Date(item.nextPickupDate.getTime() - oneDayInMilSec);
-        }
+          if(item.quantity >= twoDays){
+              numberOfPillsMinusTwoDays -= twoDays;
+          }
+
+          item.nextPickupDate = new Date(today.getTime() + (oneDayInMilSec * numberOfPillsMinusTwoDays));
+
+          while(item.nextPickupDate.getDay() == sunday || item.nextPickupDate.getDay() == saturday){
+              item.nextPickupDate = new Date(item.nextPickupDate.getTime() - oneDayInMilSec);
+          }
+    };
+
+    $scope.setUpdatePickUpForSimilarDrugRegime = function(item){
+
+           var minumumQuantity = item.quantity;
+
+           var itemComparison = item;
+
+         _.forEach($scope.selectedItems, function (selectedItem) {
+
+              if( itemComparison.drugRegime && selectedItem.drugRegime){
+
+                  if(itemComparison.drugRegime.regime.uuid == selectedItem.drugRegime.regime.uuid){
+
+                       selectedItem.quantity = itemComparison.quantity;
+                       $scope.setUpdatePickUp(selectedItem);
+
+                      if(selectedItem.quantity > itemComparison.quantity){
+
+                        selectedItem.quantity = itemComparison.quantity;
+                        $scope.setUpdatePickUp(selectedItem);
+                      }
+                      else if(selectedItem.quantity < itemComparison.quantity){
+
+                        itemComparison.quantity = selectedItem.quantity;
+                        $scope.setUpdatePickUp(itemComparison);
+
+                      }
+                  }
+              }
+         });
+
     };
 
     $scope.dispense = function() {
@@ -129,11 +222,13 @@ function DispensationController($scope, $rootScope, $filter, dispensationService
         _.forEach($scope.selectedItems, function (item) {
 
             var dispensationItem = {
-                orderUuid : item.order.uuid,
-                quantityToDispense : item.quantity ? item.quantity : item.order.quantity,
+
+                orderUuid : item.drugOrder.uuid,
+                quantityToDispense : item.quantity ? item.quantity : item.drugOrder.quantity,
                 quantityDispensed : item.drugPickedUp,
                 dateOfNextPickUp : item.nextPickupDate,
-                conceptParentUuid : item.conceptParentUuid
+                prescriptionUuid : item.drugOrder.encounter.uuid,
+                regimeUuid :(item.drugRegime) ? item.drugRegime.regime.uuid : null
             };
 
             dispensation.dispensationItems.push(dispensationItem);
