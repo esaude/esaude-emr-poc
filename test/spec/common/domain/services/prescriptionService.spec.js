99a717e87533b50f5d127fb95b8ece73e4a751f2
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-07-06 09:59:25 +0200

    Test order attributes.

--- test/spec/common/domain/services/prescriptionService.spec.js
+++ test/spec/common/domain/services/prescriptionService.spec.js
@@ -17,7 +17,7 @@ describe('prescriptionService', function () {
 
   describe('getPatientPrescriptions', function () {
 
-    var patient = { uuid: "4d499e76-618c-11e7-907b-a6006ad3dba0", age: { years: 26 } };
+    var patient = {uuid: "4d499e76-618c-11e7-907b-a6006ad3dba0", age: {years: 26}};
 
     var encounters = [
       {
@@ -29,20 +29,20 @@ describe('prescriptionService', function () {
               display: "Efavirenz (efv) 50mg 30caps (embalagem)",
               uuid: "aaaafe5a-0dd7-4543-a10f-c51cda6bb1b0"
             },
-            dose: 1,
+            dose: 50,
             doseUnits: {
-              uuid: "9d674660-10e8-11e5-9009-0242ac110012"
+              uuid: "9d68ef86-10e8-11e5-9009-0242ac110012"
             },
             frequency: {
               display: "Once a day",
               uuid: "9d714903-10e8-11e5-9009-0242ac110012"
             },
             route: {
-              uuid: "9d6b861d-10e8-11e5-9009-0242ac110012"
+              uuid: "9d6af026-10e8-11e5-9009-0242ac110012"
             },
             duration: 1,
             durationUnits: {
-              uuid: "9d96d012-10e8-11e5-9009-0242ac110012"
+              uuid: "9d956959-10e8-11e5-9009-0242ac110012"
             },
             dosingInstructions: "9d7408af-10e8-11e5-9009-0242ac110012"
           }
@@ -73,72 +73,58 @@ describe('prescriptionService', function () {
     var drugPrescriptionConvSet = [
       {
         display: "dosingUnits",
-        model: {
-          answers: [
-            {display: "ml", "uuid": "9d68af32-10e8-11e5-9009-0242ac110012"},
-            {display: "mg", "uuid": "9d68ef86-10e8-11e5-9009-0242ac110012"}
-          ]
-        },
+        answers: [
+          {display: "ml", uuid: "9d68af32-10e8-11e5-9009-0242ac110012"},
+          {display: "mg", uuid: "9d68ef86-10e8-11e5-9009-0242ac110012"}
+        ],
         uuid: "9d66a447-10e8-11e5-9009-0242ac110012"
       },
       {
         display: "drugRoute",
-        model: {
-          answers: [
-            {display: "Intramuscular", uuid: "9d6af026-10e8-11e5-9009-0242ac110012"},
-            {display: "Topical", uuid: "1e4f068f-10f5-11e5-9009-0242ac110012"}
-          ]
-        },
+        answers: [
+          {display: "Intramuscular", uuid: "9d6af026-10e8-11e5-9009-0242ac110012"},
+          {display: "Topical", uuid: "1e4f068f-10f5-11e5-9009-0242ac110012"}
+        ],
         uuid: "9d6a9238-10e8-11e5-9009-0242ac110012"
       },
       {
         display: "durationUnits",
-        model: {
-          answers: [
-            {display: "Minute(s)", uuid: "1e5705ee-10f5-11e5-9009-0242ac110012"},
-            {display: "Hour(s)", uuid: "9d956959-10e8-11e5-9009-0242ac110012"}
-          ]
-        },
+        answers: [
+          {display: "Minute(s)", uuid: "1e5705ee-10f5-11e5-9009-0242ac110012"},
+          {display: "Month (s)", uuid: "9d956959-10e8-11e5-9009-0242ac110012"}
+        ],
         uuid: "9d6f0bea-10e8-11e5-9009-0242ac110012"
       },
       {
         display: "dosingInstructions",
-        model: {
-          answers: [
-            {display: "Before meals", uuid: "9d7408af-10e8-11e5-9009-0242ac110012"},
-            {display: "Empty stomach", uuid: "9d744e5a-10e8-11e5-9009-0242ac110012"}
-          ]
-        },
+        answers: [
+          {display: "Before meals", uuid: "9d7408af-10e8-11e5-9009-0242ac110012"},
+          {display: "Empty stomach", uuid: "9d744e5a-10e8-11e5-9009-0242ac110012"}
+        ],
         uuid: "9d73c2a7-10e8-11e5-9009-0242ac110012"
       },
       {
         display: "artPlan",
-        model: {
-          answers: [
-            {display: "START DRUGS", uuid: "e1d9ef28-1d5f-11e0-b929-000c29ad1d07"},
-            {display: "STOP ALL", uuid: "e1d9f36a-1d5f-11e0-b929-000c29ad1d07"}
-          ]
-        },
+        answers: [
+          {display: "START DRUGS", uuid: "e1d9ef28-1d5f-11e0-b929-000c29ad1d07"},
+          {display: "STOP ALL", uuid: "e1d9f36a-1d5f-11e0-b929-000c29ad1d07"}
+        ],
         uuid: "e1d9ee10-1d5f-11e0-b929-000c29ad1d07"
       },
       {
         display: "interruptedReason",
-        model: {
-          answers: [
-            {display: "TOXICITY, DRUG", uuid: "e1cf3b32-1d5f-11e0-b929-000c29ad1d07"},
-            {display: "OTHER NON-CODED", uuid: "e1e783ea-1d5f-11e0-b929-000c29ad1d07"}
-          ]
-        },
+        answers: [
+          {display: "TOXICITY, DRUG", uuid: "e1cf3b32-1d5f-11e0-b929-000c29ad1d07"},
+          {display: "OTHER NON-CODED", uuid: "e1e783ea-1d5f-11e0-b929-000c29ad1d07"}
+        ],
         uuid: "e1d9ead2-1d5f-11e0-b929-000c29ad1d07"
       },
       {
         display: "changeReason",
-        model: {
-          answers: [
-            {display: "LACK OF INITIAL EFFECTIVENESS", uuid: "e1de8560-1d5f-11e0-b929-000c29ad1d07"},
-            {display: "IMMUNOLOGICAL FAILURE OF ARV TREATMENT", uuid: "e1de8768-1d5f-11e0-b929-000c29ad1d07"}
-          ]
-        },
+        answers: [
+          {display: "LACK OF INITIAL EFFECTIVENESS", uuid: "e1de8560-1d5f-11e0-b929-000c29ad1d07"},
+          {display: "IMMUNOLOGICAL FAILURE OF ARV TREATMENT", uuid: "e1de8768-1d5f-11e0-b929-000c29ad1d07"}
+        ],
         uuid: "e1de8862-1d5f-11e0-b929-000c29ad1d07"
       }
     ];
@@ -174,7 +160,21 @@ describe('prescriptionService', function () {
       });
 
       $rootScope.$apply();
-      expect(loadedPrescriptions.encounterOrders.length).toEqual(encounters[0].orders.length);
+      expect(loadedPrescriptions.encounterOrders.length).toBe(encounters.length);
+
+      var encounterOrder = loadedPrescriptions.encounterOrders[0];
+      expect(encounterOrder.orders.length).toBe(encounters[0].orders.length);
+
+      var order = encounterOrder.orders[0];
+      expect(order.doseAmount).toBe(50);
+      expect(order.dosgeFrequency.display).toBe('Once a day');
+      expect(order.dosingInstructions.display).toBe('Before meals');
+      expect(order.dosingUnits.display).toBe('mg');
+      expect(order.drug.display).toBe('Efavirenz (efv) 50mg 30caps (embalagem)');
+      expect(order.drugRoute.display).toBe('Intramuscular');
+      expect(order.duration).toBe(1);
+      expect(order.durationUnits.display).toBe('Month (s)');
+
       expect(loadedPrescriptions.todaysEncounter).toBeUndefined();
       expect(loadedPrescriptions.hasServiceToday).toBeFalsy();
       expect(loadedPrescriptions.hasEntryToday).toBeFalsy();
@@ -196,7 +196,21 @@ describe('prescriptionService', function () {
         });
 
         $rootScope.$apply();
-        expect(loadedPrescriptions.encounterOrders.length).toEqual(encounters[0].orders.length);
+        expect(loadedPrescriptions.encounterOrders.length).toBe(encounters.length);
+
+        var encounterOrder = loadedPrescriptions.encounterOrders[0];
+        expect(encounterOrder.orders.length).toBe(encounters[0].orders.length);
+
+        var order = encounterOrder.orders[0];
+        expect(order.doseAmount).toBe(50);
+        expect(order.dosgeFrequency.display).toBe('Once a day');
+        expect(order.dosingInstructions.display).toBe('Before meals');
+        expect(order.dosingUnits.display).toBe('mg');
+        expect(order.drug.display).toBe('Efavirenz (efv) 50mg 30caps (embalagem)');
+        expect(order.drugRoute.display).toBe('Intramuscular');
+        expect(order.duration).toBe(1);
+        expect(order.durationUnits.display).toBe('Month (s)');
+
         expect(loadedPrescriptions.todaysEncounter).toEqual(encounters[0]);
         expect(loadedPrescriptions.hasServiceToday).toBe(true);
         expect(loadedPrescriptions.hasEntryToday).toBe(true);
