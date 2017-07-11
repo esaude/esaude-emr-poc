396d26a9bb280f4f97e4fb7e1466d072c709cf54
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-07-11 16:26:37 +0200

    Refactor clinicalService directive.

---
+++ app/poc-common/clinical-services/directives/clinicalServices.js
@@ -0,0 +1,165 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('poc.common.clinicalservices')
+    .directive('clinicalServices', clinicalService);
+
+
+  function clinicalService() {
+    var directive = {
+      bindToController: true,
+      controller: ClinicalServiceDirectiveController,
+      controllerAs: 'vm',
+      restrict: 'AE',
+      templateUrl: ' ../poc-common/clinical-services/views/clinicalService.html',
+      scope: {
+        patientUuid: '=',
+        encounteDatetime: '=',
+        services: '='
+      }
+    };
+    return directive;
+  }
+
+  ClinicalServiceDirectiveController.$inject = ['$scope', 'encounterService', 'patientService'];
+
+  function ClinicalServiceDirectiveController($scope, encounterService, patientService) {
+
+    var dateUtil = Bahmni.Common.Util.DateUtil;
+
+    var vm = this;
+
+    vm.patient = {};
+    vm.checkRestrictionsToAdd = checkRestrictionsToAdd;
+    vm.linkAdd = linkAdd;
+    vm.linkEdit = linkEdit;
+    vm.list = list;
+    vm.$onInit = onInit;
+    vm.removeEncounter = removeEncounter;
+
+    ////////////////
+
+    function onInit() {
+      patientService.getPatient(vm.patientUuid).then(function (patient) {
+
+        vm.patient = patient;
+
+        vm.services.forEach(function (s) {
+          initService(s, patient);
+        });
+
+      });
+    }
+
+
+    function initService(service, patient) {
+      var formPayload = $scope.$parent.serviceForms[service.id];
+
+      var getEncounters = encounterService.getEncountersForEncounterType(patient.uuid, formPayload.encounterType.uuid);
+
+      // TODO: use 'getEncounters.then' after getEncountersForEncounterType is properly refactored to handle xhr failures
+      getEncounters.success(function (data) {
+          var nonVoidedEncounters = encounterService.filterRetiredEncoounters(data.results);
+          var sortedEncounters = _.sortBy(nonVoidedEncounters, function (encounter) {
+            return moment(encounter.encounterDatetime).toDate();
+          }).reverse();
+
+          if (service.markedOn) {
+            service.encountersForService = _.filter(sortedEncounters, function (e) {
+              var foundObs = _.find(e.obs, function (o) {
+                return o.concept.uuid === service.markedOn;
+              });
+              return angular.isDefined(foundObs);
+            });
+          } else {
+            service.encountersForService = sortedEncounters;
+          }
+          service.lastEncounterForService = sortedEncounters[0];
+          service.lastEncounterForServiceMarked = service.encountersForService[0];
+
+          if (service.lastEncounterForServiceMarked) {
+            if (service.markedOn) {
+              service.lastEncounterForServiceDate = _.find(service.lastEncounterForServiceMarked.obs, function (o) {
+                return o.concept.uuid === service.markedOn;
+              }).value;
+            } else {
+              service.lastEncounterForServiceDate = service.lastEncounterForServiceMarked.encounterDatetime;
+            }
+          }
+
+          service.hasEntryToday = false;
+
+          if ($scope.$parent.todayVisit && service.lastEncounterForService) {
+            service.hasEntryToday = (dateUtil.diffInDaysRegardlessOfTime($scope.$parent.todayVisit.startDatetime,
+              service.lastEncounterForService.encounterDatetime) === 0);
+          }
+          service.list = false;
+        });
+
+      checkContraints(service);
+    }
+
+
+    function checkContraints(service) {
+      service.showService = true;
+
+      if (service.constraints.requireChekin) {
+        service.showService =  $scope.$parent.hasVisitToday;
+      }
+
+      if (service.constraints.minAge &&
+        $scope.$parent.patient.age.years < service.constraints.minAge) {
+        service.showService = false;
+      }
+
+      if (service.constraints.maxAge &&
+        $scope.$parent.patient.age.years > service.constraints.maxAge) {
+        service.showService = false;
+      }
+    }
+
+
+    function checkRestrictionsToAdd(service) {
+      var canAdd;
+      if (service.maxOccur < 0 || service.encountersForService.length < service.maxOccur) {
+        canAdd = true;
+        if (service.hasEntryToday) {
+          if (service.markedOn) {
+            var foundMark = _.find(service.lastEncounterForService.obs, function (o) {
+              return o.concept.uuid === service.markedOn;
+            });
+            if (foundMark) canAdd = false;
+          } else {
+            canAdd = false;
+          }
+        }
+      }
+      return canAdd;
+    }
+
+
+    function linkAdd(service) {
+      $scope.$parent.linkServiceAdd(service);
+    }
+
+
+    function linkEdit(service, encounter) {
+      $scope.$parent.linkServiceEdit(service, encounter);
+    }
+
+
+    function list(service) {
+      (service.list) ? service.list = false : service.list = true;
+    }
+
+
+    function removeEncounter(encounter) {
+      (angular.isUndefined(encounter.delete) || encounter.delete === false) ?
+        encounter.delete = true : encounter.delete = false;
+    }
+
+  }
+
+})();
+
