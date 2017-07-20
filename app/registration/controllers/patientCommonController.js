e2eb37ca43993219ac99412a4e2a0946d8639375
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-07-20 09:36:01 +0200

    Fix #227. Register and update  patient.

--- app/registration/controllers/patientCommonController.js
+++ app/registration/controllers/patientCommonController.js
@@ -1,150 +1,193 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('registration')
-    .controller('PatientCommonController', ['$scope', '$http', '$state', 'patientAttributeService', 'patientService', 'localStorageService', 'spinner',
-        function ($scope, $http, $state, patientAttributeService, patientService, localStorageService, spinner) {
+  angular
+    .module('registration')
+    .controller('PatientCommonController', PatientCommonController);
 
-                var dateUtil = Bahmni.Common.Util.DateUtil;
+  PatientCommonController.$inject = ['$scope', '$http', '$state', 'patientAttributeService', 'patientService', 'localStorageService', 'spinner'];
 
-                var vm = this;
-                vm.patient = $scope.patient;
+  /* @ngInject */
+  function PatientCommonController($scope, $http, $state, patientAttributeService, patientService, localStorageService,
+                                   spinner) {
 
-                (function () {
-                    vm.showMessages = false;
-                    vm.today = dateUtil.getDateWithoutTime(dateUtil.now());
-                    //get patient identifier types
-                    var searchPromise = patientService.getIdentifierTypes();
+    var dateUtil = Bahmni.Common.Util.DateUtil;
 
-                    searchPromise.success(function (data) {
-                        vm.patientIdentifierTypes = data.results;
-                    });
-                    searchPromise['finally'](function () {
-                    });
-                    spinner.forPromise(searchPromise);
-                })();
+    // TODO: Remove dependency on $scope!
+    var vm = this;
+    vm.patient = $scope.patient;
+    vm.patientIdentifierTypes = [];
+    vm.showMessages = false;
+    vm.srefPrefix = $scope.srefPrefix;
+    vm.today = dateUtil.getDateWithoutTime(dateUtil.now());
 
-                vm.listRequiredIdentifiers = function () {
-                    if (!_.isEmpty(vm.patient.identifiers)) {
-                        //set identifier fieldName
-                        _.forEach (vm.patient.identifiers, function (identifier) {
-                            var fieldName = identifier.identifierType.display.trim().replace(/[^a-zA-Z0-9]/g, '');
-                            identifier.fieldName = fieldName;
-                        });
-                        return;
-                    }-
-                    patientService.getIdentifierTypes().success(function (data) {
-                        vm.patientIdentifierTypes = data.results;
-                        _.forEach (data.results, function (value) {
-                            if (value.required) {
-                                var fieldName = value.name.trim().replace(/[^a-zA-Z0-9]/g, '');
-                                vm.patient.identifiers.push({identifierType: value,
-                                    identifier: null, preferred: false,
-                                    location: localStorageService.cookie.get("emr.location").uuid,
-                                    fieldName : fieldName});
-                            }
-                        });
-                    });
-                };
+    vm.disableIsDead = disableIsDead;
+    vm.getAutoCompleteList = getAutoCompleteList;
+    vm.getDataResults = getDataResults;
+    vm.getDeathConcepts = getDeathConcepts;
+    vm.filterRetireDeathConcepts = filterRetireDeathConcepts;
+    vm.listRequiredIdentifiers = listRequiredIdentifiers;
+    vm.removeIdentifier = removeIdentifier;
+    vm.selectIdentifierType = selectIdentifierType;
+    vm.selectIsDead = selectIsDead;
+    vm.setPreferredId = setPreferredId;
+    vm.stepForward = stepForward;
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+      var load = getIdentifierTypes().then(function (identifierTypes) {
+        vm.patientIdentifierTypes = identifierTypes;
+      });
+      spinner.forPromise(load);
+    }
 
 
-                vm.selectIdentifierType = function () {
+    function disableIsDead() {
+      return (vm.patient.causeOfDeath !== null || vm.patient.deathDate !== null) && vm.patient.dead;
+    }
 
-                    vm.errorMessage = null;
 
-                    var patientIdentifierType = vm.patient.patientIdentifierType;
-                    if (patientIdentifierType !== null) {
-                        //validate already contained
-                        var found = _.find(vm.patient.identifiers, function (chr) {
-                            return chr.identifierType.display === patientIdentifierType.display;
-                        });
+    function getAutoCompleteList(attributeName, query, type) {
+      return patientAttributeService.search(attributeName, query, type);
+    }
 
-                        if(found === undefined) {
-                            var fieldName = patientIdentifierType.name.trim().replace(/[^a-zA-Z0-9]/g, '');
 
-                            vm.patient.identifiers.push({identifierType: patientIdentifierType,
-                                identifier: null, preferred: false,
-                                location: localStorageService.cookie.get("emr.location").uuid,
-                                fieldName : fieldName
-                            });
+    function getDataResults(data) {
+      return data.results;
+    }
 
-                        } else {
-                            vm.errorMessage = "PATIENT_INFO_IDENTIFIER_ERROR_EXISTING";
-                        }
-                    }
-                };
 
-                vm.removeIdentifier = function (identifier) {
-                    vm.errorMessage = null;
+    function getDeathConcepts() {
+      var deathConcept;
+      var deathConceptValue;
+      $http({
+        url: '/openmrs/ws/rest/v1/systemsetting',
+        method: 'GET',
+        params: {
+          q: 'concept.causeOfDeath',
+          v: 'full'
+        },
+        withCredentials: true,
+        transformResponse: [function (data) {
+          deathConcept = JSON.parse(data);
+          deathConceptValue = deathConcept.results[0].value;
+          $http.get(Bahmni.Common.Constants.conceptUrl, {
+            params: {
+              q: deathConceptValue,
+              v: 'custom:(uuid,name,set,answers:(uuid,display,name:(uuid,name),retired))'
+            },
+            withCredentials: true
+          }).then(function (results) {
+            vm.deathConcepts = results.data.results[0] !== null ? results.data.results[0].answers : [];
+            vm.deathConcepts = filterRetireDeathConcepts(vm.deathConcepts);
+          });
+        }]
+      });
+    }
 
-                    _.pull(vm.patient.identifiers, identifier);
-                };
 
-                vm.setPreferredId = function (identifier) {
-                    angular.forEach(vm.patient.identifiers, function (p) {
-                        if (p.identifierType.uuid !== identifier.identifierType.uuid) {
-                            p.preferred = false; //set them all to false
-                        }
-                    });
-                };
+    function filterRetireDeathConcepts(deathConcepts) {
+      return _.filter(deathConcepts, function (concept) {
+        return !concept.retired;
+      });
+    }
 
-                vm.stepForward = function (sref, validity) {
-                    if (validity) {
-                        vm.showMessages = false;
-                        $state.go(vm.srefPrefix + sref);
-                    } else {
-                        vm.showMessages = true;
-                    }
-                };
 
-                vm.getAutoCompleteList = function (attributeName, query, type) {
-                    return patientAttributeService.search(attributeName, query, type);
-                };
+    function listRequiredIdentifiers() {
+      if (!_.isEmpty(vm.patient.identifiers)) {
+        //set identifier fieldName
+        _.forEach(vm.patient.identifiers, function (identifier) {
+          identifier.fieldName = identifier.identifierType.display.trim().replace(/[^a-zA-Z0-9]/g, '');
+        });
+        return;
+      }
 
-                vm.getDataResults = function (data) {
-                    return  data.results;
-                };
+      getIdentifierTypes().then(function (identifierTypes) {
+        vm.patientIdentifierTypes = identifierTypes;
+        _.forEach(identifierTypes, function (value) {
+          if (value.required) {
+            var fieldName = value.name.trim().replace(/[^a-zA-Z0-9]/g, '');
+            vm.patient.identifiers.push({
+              identifierType: value,
+              identifier: null, preferred: false,
+              location: localStorageService.cookie.get("emr.location").uuid,
+              fieldName: fieldName
+            });
+          }
+        });
+      });
+    }
 
-                vm.getDeathConcepts = function () {
-                    var deathConcept;
-                    var deathConceptValue;
-                    $http({
-                        url: '/openmrs/ws/rest/v1/systemsetting',
-                        method: 'GET',
-                        params: {
-                            q: 'concept.causeOfDeath',
-                            v: 'full'
-                        },
-                        withCredentials: true,
-                        transformResponse: [function(data){
-                            deathConcept = JSON.parse(data);
-                            deathConceptValue = deathConcept.results[0].value;
-                            $http.get(Bahmni.Common.Constants.conceptUrl, {
-                                params: {
-                                    q: deathConceptValue,
-                                    v: 'custom:(uuid,name,set,answers:(uuid,display,name:(uuid,name),retired))'
-                                },
-                                withCredentials: true
-                            }).then(function (results) {
-                                vm.deathConcepts = results.data.results[0]!=null ? results.data.results[0].answers:[];
-                                vm.deathConcepts = filterRetireDeathConcepts(vm.deathConcepts);
-                            });
-                        }]
-                    });
-                };
 
-                var filterRetireDeathConcepts = function(deathConcepts){
-                    return _.filter(deathConcepts,function(concept){
-                        return !concept.retired;
-                    });
-                };
+    function removeIdentifier(identifier) {
+      vm.errorMessage = null;
 
-                vm.selectIsDead = function(){
-                    if(vm.patient.causeOfDeath != null ||vm.patient.deathDate != null){
-                        vm.patient.dead = true;
-                    }
-                };
+      _.pull(vm.patient.identifiers, identifier);
+    }
 
-                vm.disableIsDead = function(){
-                    return (vm.patient.causeOfDeath != null || vm.patient.deathDate != null) && vm.patient.dead;
-                };
-        }]);
+
+    function selectIdentifierType() {
+
+      vm.errorMessage = null;
+
+      var patientIdentifierType = vm.patient.patientIdentifierType;
+      if (patientIdentifierType !== null) {
+        //validate already contained
+        var found = _.find(vm.patient.identifiers, function (chr) {
+          return chr.identifierType.display === patientIdentifierType.display;
+        });
+
+        if (!found) {
+          var fieldName = patientIdentifierType.name.trim().replace(/[^a-zA-Z0-9]/g, '');
+
+          vm.patient.identifiers.push({
+            identifierType: patientIdentifierType,
+            identifier: null, preferred: false,
+            location: localStorageService.cookie.get("emr.location").uuid,
+            fieldName: fieldName
+          });
+
+        } else {
+          vm.errorMessage = "PATIENT_INFO_IDENTIFIER_ERROR_EXISTING";
+        }
+      }
+    }
+
+
+    function selectIsDead() {
+      if (vm.patient.causeOfDeath !== null || vm.patient.deathDate !== null) {
+        vm.patient.dead = true;
+      }
+    }
+
+
+    function setPreferredId(identifier) {
+      angular.forEach(vm.patient.identifiers, function (p) {
+        if (p.identifierType.uuid !== identifier.identifierType.uuid) {
+          p.preferred = false; //set them all to false
+        }
+      });
+    }
+
+
+    function stepForward(sref, validity) {
+      if (validity) {
+        vm.showMessages = false;
+        $state.go(vm.srefPrefix + sref);
+      } else {
+        vm.showMessages = true;
+      }
+    }
+
+
+    function getIdentifierTypes() {
+      return patientService.getIdentifierTypes();
+    }
+
+  }
+
+})();
+
