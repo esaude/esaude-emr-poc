32027405bf57acd6f885898134ef6418062dbc16
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-08-11 12:05:09 +0200

    Refactor home routes.

--- app/home/app.js
+++ app/home/app.js
@@ -1,31 +1,52 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular
+  angular
     .module('home')
-    .config(['$urlRouterProvider', '$stateProvider', '$bahmniTranslateProvider', '$httpProvider',
-                function ($urlRouterProvider, $stateProvider, $bahmniTranslateProvider, $httpProvider) {
-        // to prevent the browser from displaying a password pop-up in case of an authentication error
-        $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = 'true';
-        $urlRouterProvider.otherwise(function ($injector) {
-            var $state = $injector.get('$state');
-            $state.go('dashboard');
-        });
-        
-        $stateProvider
-            .state('login', {
-                url: '/login?showLoginMessage=LOGIN_LABEL_LOGIN_ERROR_MESSAGE_KEY',
-                views: {
-                    'layout': { templateUrl: '../common/application/views/layout.html', controller: 'LoginController'},
-                    'content@login': { templateUrl: 'views/login.html'}
-                }
-            })
-            .state('dashboard', {
-                url: '/dashboard',
-                views: {
-                    'layout': { templateUrl: '../common/application/views/layout.html', controller: 'DashboardController'},
-                    'content@dashboard': { templateUrl: 'views/dashboard.html'}
-                },
-                resolve: { initialization: 'initialization' }
-            });
-            $bahmniTranslateProvider.init({app: 'home', shouldMerge: true});
-    }]);
+    .config(config);
+
+  config.$inject = ['$urlRouterProvider', '$stateProvider', '$bahmniTranslateProvider', '$httpProvider'];
+
+  /* @ngInject */
+  function config($urlRouterProvider, $stateProvider, $bahmniTranslateProvider, $httpProvider) {
+    // to prevent the browser from displaying a password pop-up in case of an authentication error
+    $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = 'true';
+    $urlRouterProvider.otherwise(function ($injector) {
+      var $state = $injector.get('$state');
+      $state.go('dashboard');
+    });
+
+    $stateProvider
+      .state('login', {
+        url: '/login?showLoginMessage=LOGIN_LABEL_LOGIN_ERROR_MESSAGE_KEY',
+        views: {
+          'layout': {
+            templateUrl: '../common/application/views/layout.html',
+            controller: 'LoginController',
+            controllerAs: 'vm'
+          },
+          'content@login': {
+            templateUrl: 'views/login.html'
+          }
+        }
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        views: {
+          'layout': {
+            templateUrl: '../common/application/views/layout.html',
+            controller: 'DashboardController'
+          },
+          'content@dashboard': {
+            templateUrl: 'views/dashboard.html'
+          }
+        },
+        resolve: {
+          initialization: 'initialization'
+        }
+      });
+
+    $bahmniTranslateProvider.init({app: 'home', shouldMerge: true});
+  }
+
+})();
