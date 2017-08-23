41ad52fd4c73fc6a2b5ff51dc7aae540bfdc9ae7
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-08-23 13:44:13 +0200

    Refactor application module and karma config.

--- app/common/application/init.js
+++ app/common/application/init.js
@@ -1,40 +1,41 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular
-  .module('application',
-    [
+  angular
+    .module('application', [
       'authentication',
       'ncy-angular-breadcrumb'
-    ]
-  )
-  .config(applicationModuleConfig);
+    ])
+    .config(applicationModuleConfig);
 
-applicationModuleConfig.$inject = ['$provide', '$breadcrumbProvider'];
+  applicationModuleConfig.$inject = ['$provide', '$breadcrumbProvider'];
 
-/* @ngInject */
-function applicationModuleConfig($provide, $breadcrumbProvider) {
-  // $provide.decorator('applicationService', applicationServiceAuthorizationDecorator);
+  /* @ngInject */
+  function applicationModuleConfig($provide, $breadcrumbProvider) {
+    // $provide.decorator('applicationService', applicationServiceAuthorizationDecorator);
 
-  $breadcrumbProvider.setOptions({
-    templateUrl: '/common/application/views/breadcrumb.html'
-  });
-}
-
-applicationServiceAuthorizationDecorator.$inject = ['$delegate', 'authorizationService', '$log'];
-
-/* @ngInject */
-function applicationServiceAuthorizationDecorator($delegate, authorizationService, $log) {
-
-  $log.info('applicationServiceAuthorizationDecorator: decorating applicationService with authorization.');
-
-  var getApps = $delegate.getApps;
-
-  function getAuthorizedApps() {
-    return getApps().then(function (applications) {
-      return authorizationService.authorizeApps(applications);
+    $breadcrumbProvider.setOptions({
+      templateUrl: '/common/application/views/breadcrumb.html'
     });
   }
 
-  $delegate.getApps = getAuthorizedApps;
-  return $delegate;
-}
+  applicationServiceAuthorizationDecorator.$inject = ['$delegate', 'authorizationService', '$log'];
+
+  /* @ngInject */
+  function applicationServiceAuthorizationDecorator($delegate, authorizationService, $log) {
+
+    $log.info('applicationServiceAuthorizationDecorator: decorating applicationService with authorization.');
+
+    var getApps = $delegate.getApps;
+
+    function getAuthorizedApps() {
+      return getApps().then(function (applications) {
+        return authorizationService.authorizeApps(applications);
+      });
+    }
+
+    $delegate.getApps = getAuthorizedApps;
+    return $delegate;
+  }
+
+})();
