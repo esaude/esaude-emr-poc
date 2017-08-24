3460e09f2bffb3183ae722e9c1163ab6cb87fb35
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-08-24 16:09:28 +0200

    Breadcrumbs for social module.

--- app/social/app.js
+++ app/social/app.js
@@ -1,60 +1,102 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular
+  angular
     .module('social')
-    .config(['$urlRouterProvider', '$stateProvider', '$bahmniTranslateProvider', '$httpProvider',
-                function ($urlRouterProvider, $stateProvider, $bahmniTranslateProvider, $httpProvider) {
+    .config(config);
 
-        // to prevent the browser from displaying a password pop-up in case of an authentication error
-        $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = 'true';
-        $urlRouterProvider.otherwise(function ($injector) {
-            var localStorageService = $injector.get('localStorageService');
-            var movingPatient = localStorageService.get('movingPatient');
-            var $state = $injector.get('$state');
+  config.$inject = ['$urlRouterProvider', '$stateProvider', '$bahmniTranslateProvider', '$httpProvider'];
 
-            if (movingPatient !== null) {
-                $state.go('mvp');
-            } else {
-                $state.go('search');
-            }
-        });
+  /* @ngInject */
+  function config($urlRouterProvider, $stateProvider, $bahmniTranslateProvider, $httpProvider) {
 
-        $stateProvider
-            .state('mvp', {
-                views: {
-                    'layout': { template: '<div id="overlay"><div></div></div>', controller: 'MovePatientController'}
-                },
-                resolve: { initialization: 'initialization' }
-            })
-            .state('search', {
-                url: '/search',
-                views: {
-                    'layout': { templateUrl: '../common/application/views/layout.html', controller: 'SearchController'},
-                    'content@search': { templateUrl: 'views/search.html'}
-                },
-                resolve: { initialization: 'initialization' }
-            })
-            .state('dashboard', {
-                url: '/dashboard/:patientUuid',
-                views: {
-                    'layout': { templateUrl: '../common/application/views/layout.html', controller: 'DashboardController'},
-                    'content@dashboard': { templateUrl: 'views/dashboard.html'}
-                },
-                resolve: { initialization: 'initialization' }
-            })
-            .state('detailpatient', {
-              url: '/patient/detail/:patientUuid',
-              views: {
-                'layout': {
-                  templateUrl: '../common/application/views/layout.html',
-                  controller: 'DetailPatientController',
-                  controllerAs: 'patientCommon'
-                },
-                'content@detailpatient': {templateUrl: '../patient-details/views/patient-details.html'}
-              },
-              resolve: {initialization: 'initialization'}
-            });
+    // to prevent the browser from displaying a password pop-up in case of an authentication error
+    $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = 'true';
+    $urlRouterProvider.otherwise(function ($injector) {
+      var localStorageService = $injector.get('localStorageService');
+      var movingPatient = localStorageService.get('movingPatient');
+      var $state = $injector.get('$state');
 
-            $stateProviderRef = $stateProvider;
-            $bahmniTranslateProvider.init({app: 'social', shouldMerge: true});
-    }]);
+      if (movingPatient !== null) {
+        $state.go('mvp');
+      } else {
+        $state.go('search');
+      }
+    });
+
+    // used in formStateAutoGen
+    // TODO: check if formStateAutoGen can be run in angular configuration phase to eliminating this hack!!!
+    $stateProviderRef = $stateProvider;
+    $bahmniTranslateProvider.init({app: 'social', shouldMerge: true});
+
+    $stateProvider
+      .state('mvp', {
+        views: {
+          'layout': {
+            template: '<div id="overlay"><div></div></div>',
+            controller: 'MovePatientController'
+          }
+        },
+        resolve: {
+          initialization: 'initialization'
+        }
+      })
+      .state('search', {
+        url: '/search',
+        views: {
+          'layout': {
+            templateUrl: '../common/application/views/layout.html',
+            controller: 'SearchController'
+          },
+          'content@search': {
+            templateUrl: 'views/search.html'
+          }
+        },
+        resolve: {
+          initialization: 'initialization'
+        },
+        ncyBreadcrumb: {
+          label: '{{\'SEARCH_PATIENT\' | translate}}'
+        }
+      })
+      .state('dashboard', {
+        url: '/dashboard/:patientUuid',
+        views: {
+          'layout': {
+            templateUrl: '../common/application/views/layout.html',
+            controller: 'DashboardController'
+          },
+          'content@dashboard': {
+            templateUrl: 'views/dashboard.html'
+          }
+        },
+        resolve: {
+          initialization: 'initialization'
+        },
+        ncyBreadcrumb: {
+          label: '{{ \'COMMON_DASHBOARD\' | translate}}',
+          parent: 'search'
+        }
+      })
+      .state('detailpatient', {
+        url: '/patient/detail/:patientUuid',
+        views: {
+          'layout': {
+            templateUrl: '../common/application/views/layout.html',
+            controller: 'DetailPatientController',
+            controllerAs: 'patientCommon'
+          },
+          'content@detailpatient': {
+            templateUrl: '../patient-details/views/patient-details.html'
+          }
+        },
+        resolve: {
+          initialization: 'initialization'
+        },
+        ncyBreadcrumb: {
+          label: '{{\'PATIENT_DETAILS\' | translate }}',
+          parent: 'dashboard'
+        }
+      });
+  }
+})();
