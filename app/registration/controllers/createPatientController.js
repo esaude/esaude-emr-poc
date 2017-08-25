bd007dad3e051a0d69cb2ad7753c3fcc0abc5394
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-08-25 11:49:18 +0200

    Fix cancel.

--- app/registration/controllers/createPatientController.js
+++ app/registration/controllers/createPatientController.js
@@ -1,39 +1,54 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('registration')
-    .controller('CreatePatientController', ['$rootScope', '$scope', '$location', '$filter', 'patient', 'patientService',
-                    'appService', 'openmrsPatientMapper', 'notifier',
-        function ($rootScope, $scope, $location, $filter, patientModel, patientService, appService, patientMapper, notifier) {
+  angular
+    .module('registration')
+    .controller('CreatePatientController', CreatePatientController);
 
-                $scope.actions = {};
-                $scope.addressHierarchyConfigs = appService.getAppDescriptor().getConfigValue("addressHierarchy");
+  CreatePatientController.$inject = ['$rootScope', '$scope', '$location', '$filter', 'patient', 'patientService',
+    'appService', 'openmrsPatientMapper', 'notifier'];
 
-                (function () {
-                    $scope.srefPrefix = "newpatient.";
-                    $scope.patient = patientModel.create();
-                })();
+  /* @ngInject */
+  function CreatePatientController($rootScope, $scope, $location, $filter, patient, patientService, appService,
+                                   openmrsPatientMapper, notifier) {
 
-                $scope.save = function () {
-                    var errMsg = Bahmni.Common.Util.ValidationUtil.validate($scope.patient,$scope.patientConfiguration.personAttributeTypes);
-                    if(errMsg){
-    //                   messagingService.showMessage('formError', errMsg);
-                        return;
-                    }
+    $scope.actions = {};
+    $scope.addressHierarchyConfigs = appService.getAppDescriptor().getConfigValue("addressHierarchy");
+    $scope.patient = patient.create();
+    $scope.srefPrefix = "newpatient.";
 
-                    patientService.create($scope.patient).success(successCallback).error(errorCallback);
-                };
+    $scope.linkSearch = linkSearch;
+    $scope.save = save;
 
-                var successCallback = function (patientProfileData) {
-                    $rootScope.patient = patientMapper.map(patientProfileData.patient);
-                    $scope.patient.uuid = patientProfileData.patient.uuid;
-                    $scope.patient.name = patientProfileData.patient.person.names[0].display;
-                    $scope.patient.isNew = true;
-                    var successMessage = $filter('translate')('COMMON_MESSAGE_SUCCESS_ACTION_COMPLETED');
-                    notifier.success(successMessage);
-                    $location.url("/dashboard/" + $scope.patient.uuid);
-                };
+    ////////////////
 
-                var errorCallback = function (data, status) {
-                    notifier.error($filter('translate')('COMMON_MESSAGE_ERROR_ACTION'));
-                };
-        }]);
+    function linkSearch() {
+      $location.url("/search");
+    }
+
+    function save() {
+      var errMsg = Bahmni.Common.Util.ValidationUtil.validate($scope.patient,$scope.patientConfiguration.personAttributeTypes);
+      if(errMsg){
+        return;
+      }
+
+      patientService.create($scope.patient).success(successCallback).error(errorCallback);
+    }
+
+    function successCallback(patientProfileData) {
+      $rootScope.patient = openmrsPatientMapper.map(patientProfileData.patient);
+      $scope.patient.uuid = patientProfileData.patient.uuid;
+      $scope.patient.name = patientProfileData.patient.person.names[0].display;
+      $scope.patient.isNew = true;
+      var successMessage = $filter('translate')('COMMON_MESSAGE_SUCCESS_ACTION_COMPLETED');
+      notifier.success(successMessage);
+      $location.url("/dashboard/" + $scope.patient.uuid);
+    }
+
+    function errorCallback() {
+      notifier.error($filter('translate')('COMMON_MESSAGE_ERROR_ACTION'));
+    }
+
+  }
+
+})();
