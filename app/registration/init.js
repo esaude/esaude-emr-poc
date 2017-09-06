ed55f960caa39f70ee193e36bac73359d52bf270
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-09-06 15:55:24 +0200

    Adjust clinical services for registration.

--- app/registration/init.js
+++ app/registration/init.js
@@ -1,33 +1,29 @@
 angular
   .module('registration',
     [
-      'ngRoute',
-      'ngCookies',
-      'ui.router',
-      'smart-table',
-      'bahmni.common.uiHelper',
+      'application',
+      'bahmni.common.appFramework',
       'bahmni.common.config',
       'bahmni.common.domain',
-      'bahmni.common.appFramework',
-      'LocalStorageModule',
-      'datePicker',
-      'frapontillo.bootstrap-switch',
-      'bahmni.common.uicontrols.programmanagment',
-      'serviceform',
-      'ui.checkbox',
-      'application',
-      'ngMessages',
-      'poc.common.clinicalservices',
-      'common.patient',
-      'pascalprecht.translate',
       'bahmni.common.i18n',
-      'patient.details',
-      'ui.bootstrap',
+      'bahmni.common.uicontrols.programmanagment',
+      'bahmni.common.uiHelper',
       'barcode',
       'barcodeListener',
-      'ngDialog'
+      'common.patient',
+      'datePicker',
+      'frapontillo.bootstrap-switch',
+      'LocalStorageModule',
+      'ngCookies',
+      'ngDialog',
+      'ngMessages',
+      'ngRoute',
+      'pascalprecht.translate',
+      'patient.details',
+      'poc.common.clinicalservices',
+      'smart-table',
+      'ui.bootstrap',
+      'ui.checkbox',
+      'ui.router'
     ]
   );
-//to be initialized by config inside app.js
-//needed in dynamic states
-var $stateProviderRef = null;
