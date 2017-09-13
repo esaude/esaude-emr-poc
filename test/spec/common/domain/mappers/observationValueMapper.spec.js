7daaba76c4bd3d74a13f51c1dbf5fd3e5e3854ba
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-09-13 11:16:34 +0200

    Delete ununsed.

--- test/spec/common/domain/mappers/observationValueMapper.spec.js
+++
@@ -1,8 +0,0 @@
-xdescribe("Obcservation Value Mapper", function () {
-  var mapper = Bahmni.Common.Domain.ObservationValueMapper;
-
-  it("should return date value", function () {
-    var obs = {type: "Date", value:"2015-12-19", concept: {}};
-    expect(mapper.map(obs)).toBe("19-Dec-2015");
-  });
-});
