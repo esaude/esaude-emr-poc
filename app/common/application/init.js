0f36ebe6a3253424a2e7f40e83b176f6c967b2fc
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-10-08 19:52:02 +0200

    Fix link.

--- app/common/application/init.js
+++ app/common/application/init.js
@@ -3,7 +3,7 @@
 
   /**
    * Top level module, application wide configuration goes here.
-   * Ideally it should load all the other modules as described here: {@link https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md#modules}.
+   * Ideally it should load all the other modules as described here: {@link https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md#module-dependencies}.
    * For now we'll let all other modules load this one, because it has as similar end-result.
    */
   angular
