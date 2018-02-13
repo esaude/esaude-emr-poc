9086bee6367ae8a6b24c224729d819b64fbb7953
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2018-02-13 10:12:40 +0200

    Refactor initialization.

--- app/vitals/initialization.js
+++ app/vitals/initialization.js
@@ -1,36 +1,46 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('vitals').factory('initialization',
-    ['$cookies', '$rootScope', 'configurations', 'authenticator', 'appService', 'spinner', 'userService',
-    function ($cookies, $rootScope, configurations, authenticator, appService, spinner, userService) {
-        var getConfigs = function () {
-            var configNames = ['patientAttributesConfig', 'addressLevels'];
-            return configurations.load(configNames).then(function () {
-                var mandatoryPersonAttributes = appService.getAppDescriptor().getConfigValue("mandatoryPersonAttributes");
-                var patientAttributeTypes = new Poc.Patient.PatientAttributeTypeMapper().mapFromOpenmrsPatientAttributeTypes(configurations.patientAttributesConfig(), mandatoryPersonAttributes);
-                $rootScope.patientConfiguration = new Poc.Patient.PatientConfig(patientAttributeTypes.personAttributeTypes, appService.getAppDescriptor().getConfigValue("additionalPatientInformation"));
-                $rootScope.encounterTypes = appService.getAppDescriptor().getConfigValue("encounterTypes");
-                $rootScope.landingPageAfterSearch = appService.getAppDescriptor().getConfigValue("landingPageAfterSearch");
-                $rootScope.defaultVisitTypes = appService.getAppDescriptor().getConfigValue("defaultVisitTypes");
-                $rootScope.appId = appService.getAppDescriptor().getId();
-            });
-        };
+  angular
+    .module('vitals')
+    .factory('initialization', initialization);
 
-        var initApp = function() {
-            return appService.initApp('vitals', {'app': true, 'extension' : true, 'service': true });
-        };
+  initialization.$inject = ['$cookies', '$rootScope', 'configurations', 'authenticator', 'appService', 'spinner', 'userService'];
 
-        var loadUser = function () {
-            var currentUser = $cookies.get(Bahmni.Common.Constants.currentUser);
+  /* @ngInject */
+  function initialization($cookies, $rootScope, configurations, authenticator, appService, spinner, userService) {
 
-            return userService.getUser(currentUser).success(function(data) {
-                $rootScope.currentUser = data.results[0];
-            });
-        };
+    return spinner.forPromise(authenticator.authenticateUser()
+      .then(initApp)
+      .then(getConfigs)
+      .then(loadUser));
 
-        return spinner.forPromise(authenticator.authenticateUser()
-                .then(initApp)
-                .then(getConfigs)
-                .then(loadUser));
-    }]
-);
+    ////////////////
+
+    function getConfigs() {
+      var configNames = ['patientAttributesConfig', 'addressLevels'];
+      return configurations.load(configNames).then(function () {
+        var mandatoryPersonAttributes = appService.getAppDescriptor().getConfigValue("mandatoryPersonAttributes");
+        var patientAttributeTypes = new Poc.Patient.PatientAttributeTypeMapper().mapFromOpenmrsPatientAttributeTypes(configurations.patientAttributesConfig(), mandatoryPersonAttributes);
+        $rootScope.patientConfiguration = new Poc.Patient.PatientConfig(patientAttributeTypes.personAttributeTypes, appService.getAppDescriptor().getConfigValue("additionalPatientInformation"));
+        $rootScope.encounterTypes = appService.getAppDescriptor().getConfigValue("encounterTypes");
+        $rootScope.landingPageAfterSearch = appService.getAppDescriptor().getConfigValue("landingPageAfterSearch");
+        $rootScope.defaultVisitTypes = appService.getAppDescriptor().getConfigValue("defaultVisitTypes");
+        $rootScope.appId = appService.getAppDescriptor().getId();
+      });
+    }
+
+    function initApp() {
+      return appService.initApp('vitals', {'app': true, 'extension': true, 'service': true});
+    }
+
+    function loadUser() {
+      var currentUser = $cookies.get(Bahmni.Common.Constants.currentUser);
+
+      return userService.getUser(currentUser).success(function (data) {
+        $rootScope.currentUser = data.results[0];
+      });
+    }
+  }
+
+})();
