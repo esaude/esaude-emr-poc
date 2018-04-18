9211d395e7c387b5653859e03e9a7094613556be
Guimino <minoneves@gmail.com>
2018-04-18 21:51:05 +0200

    added a wrapper resource to method getEncountersForEncounterType of EncounterService to resolve classCastException

--- app/common/domain/services/encounterService.js
+++ app/common/domain/services/encounterService.js
@@ -166,7 +166,7 @@
       if (!v) {
         v = "custom:(uuid,encounterDatetime,provider,voided,visit:(uuid,startDatetime,stopDatetime),obs:(uuid,concept:(uuid,name),obsDatetime,value,groupMembers:(uuid,concept:(uuid,name),order:(uuid,voided,drug,quantity,dose,doseUnits,frequency,quantityUnits,dosingInstructions,duration,durationUnits,route),obsDatetime,value)))";
       }
-      return $http.get(Bahmni.Common.Constants.encounterUrl, {
+      return $http.get(Bahmni.Common.Constants.encounterWrapUrl, {
         params: {
           patient: patientUuid,
           encounterType: encounterTypeUuid,
