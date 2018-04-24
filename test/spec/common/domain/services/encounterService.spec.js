058de2fb38d3131991fb51cf34636c8d9e025cea
Valério João <valeriovjoao@gmail.com>
2018-04-24 09:54:24 +0200

    Add condition to call encounter with and without orders. Fix #477 (#482)

--- test/spec/common/domain/services/encounterService.spec.js
+++ test/spec/common/domain/services/encounterService.spec.js
@@ -27,4 +27,44 @@ describe('encounterService', function () {
     });
   });
 
+  describe('get', function () {
+
+    it('should get encounter by encounter type and representation', function () {
+
+      var patient = "aaa-bbb-ccc";
+      var encounterType = "ccc-aaa-bbb";
+      var urlFull= "/openmrs/ws/rest/v1/encounterwrap?encounterType="+ encounterType + "&patient="+ patient + "&v=custom:(uuid,encounterDatetime,provider,voided,visit:(uuid,startDatetime,stopDatetime),obs:(uuid,concept:(uuid,name),obsDatetime,value,groupMembers:(uuid,concept:(uuid,name),order:(uuid,voided,drug,quantity,dose,doseUnits,frequency,quantityUnits,dosingInstructions,duration,durationUnits,route),obsDatetime,value)))";
+      $httpBackend.expectGET(urlFull, {"Accept":"application/json, text/plain, */*"}).respond({});
+
+      var encounter;
+      encounterService.getEncountersForEncounterType(patient, encounterType).then(function (created) {
+        encounter = created;
+      });
+
+      $httpBackend.flush();
+
+      expect(encounter.data).toEqual({});
+
+    });
+
+    it('should get encounter by encounter type', function () {
+
+      var patient = "aaa-bbb-ccc";
+      var encounterType = "ccc-aaa-bbb";
+      var urlFull= "/openmrs/ws/rest/v1/encounter?encounterType="+ encounterType + "&patient="+ patient + "&v=custom:(uuid,encounterDatetime,provider,voided,obs:(uuid,concept:(uuid,name),obsDatetime,value,groupMembers:(uuid,concept:(uuid,name),obsDatetime,value)))"
+      $httpBackend.expectGET(urlFull, {"Accept":"application/json, text/plain, */*"}).respond({});
+
+      var encounter;
+      encounterService.getEncountersForEncounterType(patient, encounterType, "default").then(function (created) {
+        encounter = created;
+      });
+
+      $httpBackend.flush();
+
+      expect(encounter.data).toEqual({});
+
+    });
+
+  });
+
 });
