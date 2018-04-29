37012c045da2ee5632107bb3f7a9fcf34f0562d2
Yassin <yassin791@gmail.com>
2018-04-29 19:14:24 +0200

    Change scheduled list title (#557)
    * Change scheduled list the title
    
    Fixes #531.
    
    * Coverage.

--- app/common/patient/components/scheduleList.js
+++ app/common/patient/components/scheduleList.js
@@ -3,30 +3,18 @@
 
   angular
     .module('common.patient')
-    .directive('scheduleList', scheduleList);
-
-  scheduleList.$inject = [];
-
-  /* @ngInject */
-  function scheduleList() {
-    var directive = {
-      bindToController: true,
+    .component('scheduleList', {
       controller: ScheduleListController,
       controllerAs: 'vm',
-      restrict: 'E',
-      scope: {
+      bindings: {
         onPatientSelect: '&',
-        scheduleType: '='
+        scheduleType: '<'
       },
-      templateUrl: '../common/patient/directives/scheduleList.html'
-    };
-    return directive;
-  }
-
-  ScheduleListController.$inject = ['scheduleService'];
+      templateUrl: '../common/patient/components/scheduleList.html'
+    });
 
   /* @ngInject */
-  function ScheduleListController(scheduleService) {
+  function ScheduleListController(scheduleService, translateFilter) {
 
     var SCHEDULE_TYPE_CURRENT_PROVIDER = 'currentProvider';
     var SCHEDULE_TYPE_DRUG_PICKUP = 'drugPickup';
@@ -34,6 +22,7 @@
     var vm = this;
 
     vm.patients = [];
+    vm.title = translateFilter('SCHEDULE_LIST_PATIENT');
 
     activate();
 
@@ -44,8 +33,10 @@
 
       if (vm.scheduleType === SCHEDULE_TYPE_CURRENT_PROVIDER) {
         getSchedule = scheduleService.getCurrentProviderSchedule();
+        vm.title = translateFilter('SCHEDULE_LIST_PROVIDER');
       } else if (vm.scheduleType === SCHEDULE_TYPE_DRUG_PICKUP) {
         getSchedule = scheduleService.getDrugPickupSchedule();
+        vm.title = translateFilter('SCHEDULE_LIST_DRUG_PICKUP');
       } else {
         getSchedule = scheduleService.getPatientSchedule();
       }
