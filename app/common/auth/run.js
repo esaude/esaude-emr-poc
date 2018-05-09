7f656700e06d14428834019f6481136b07483b0f
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2018-05-09 10:19:29 +0200

    Revert "Show message when session expired."
    This reverts commit 9ebfefc67061f7e489375f4b5bcd797049913232.

--- app/common/auth/run.js
+++ app/common/auth/run.js
@@ -8,11 +8,13 @@
   run.$inject = ['$rootScope', '$window', '$timeout', 'sessionService'];
 
   function run($rootScope, $window, $timeout, sessionService) {
-    var unregister = $rootScope.$on('event:auth-loginRequired', function (ev, message) {
+    var unregister = $rootScope.$on('event:auth-loginRequired', function () {
       $timeout(function () {
-        sessionService.destroy().then(function () {
-          $window.location = "../home/index.html#/login?showLoginMessage=" + message;
-        });
+        sessionService.destroy().then(
+          function () {
+            $window.location = "../home/index.html#/login";
+          }
+        );
       });
     });
     $rootScope.$on('$destroy', unregister);
