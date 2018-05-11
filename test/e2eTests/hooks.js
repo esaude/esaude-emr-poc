b9c10a3d755727164051a6b62b25f7ad53a775f1
drryanjames <rcjames1004@gmail.com>
2018-05-11 05:07:32 -0700

    Catch errors thrown during cleanup

--- test/e2eTests/hooks.js
+++ test/e2eTests/hooks.js
@@ -3,9 +3,13 @@ const Apis = require('./rest/openMrsApis')
 
 module.exports = function() {
 	// Fired after every test
-  event.dispatcher.on(event.test.after, function (test) {
-  	// Remove any and all data that was injected into the DB
-  	// after each test
-  	return Apis.cleanUp()
+  event.dispatcher.on(event.test.after, async (test) => {
+  	try{
+  		// Remove any and all data that was injected into the DB
+	  	// after each test
+	  	await Apis.cleanUp()
+  	} catch (err) {
+  		console.log(`The following error was thrown while cleaning up injected data: ${err}`)
+  	}
   })
 }
