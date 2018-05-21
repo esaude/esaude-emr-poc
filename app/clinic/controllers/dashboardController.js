2ea5f6a1715685165c9c0cb090a94e619b79a95f
Edrisse Mussá <edrisseasm@gmail.com>
2018-05-21 10:37:13 +0200

    assuming no privileges initially to be more safe

--- app/clinic/controllers/dashboardController.js
+++ app/clinic/controllers/dashboardController.js
@@ -6,16 +6,16 @@
     .controller('DashboardController', DashboardController);
 
   DashboardController.$inject = ['$rootScope', '$scope', '$state', '$stateParams', 'patientService', 'alertService',
-    'visitService', 'authorizationService'];
+    'visitService', 'authorizationService', '$q'];
 
   /* @ngInject */
   function DashboardController($rootScope, $scope, $state, $stateParams, patientService, alertService, visitService,
-    authorizationService) {
+    authorizationService, $q) {
 
     $scope.flags = [];
     $scope.patientUUID = $stateParams.patientUuid;
     $scope.todayVisit = null;
-    $scope.hasLabOrderPrivilege = true;
+    $scope.hasLabOrderPrivilege = false;
     $scope.patient = {};
 
     $scope.reload = reload;
@@ -50,13 +50,19 @@
     }
 
     function checkLabOrderPrivilege() {
+      var privilegeCheckPromises = [];
       var privilegesToCheck = ['Write Test Order', 'Read Test Order', 'Edit Test Order'];
       privilegesToCheck.forEach(function (privilege) {
-        authorizationService.hasPrivilege(privilege).then(function (hasPrivilege) {
-          if (!hasPrivilege) {
-            $scope.hasLabOrderPrivilege = false;
+        privilegeCheckPromises.push(authorizationService.hasPrivilege(privilege));
+      });
+      $q.all(privilegeCheckPromises).then(function (privileges) {
+        var grantAccess = true;
+        privileges.forEach(function (privilege) {
+          if (!privilege) {
+            grantAccess = false;
           }
         });
+        $scope.hasLabOrderPrivilege = grantAccess;
       });
     }
   }
