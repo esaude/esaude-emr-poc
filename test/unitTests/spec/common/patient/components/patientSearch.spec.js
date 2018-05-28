103ed61077733e3970bc2622f8801c93eb062ffa
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2018-05-28 11:17:10 +0200

    Mock barcode listener.

--- test/unitTests/spec/common/patient/components/patientSearch.spec.js
+++ test/unitTests/spec/common/patient/components/patientSearch.spec.js
@@ -4,6 +4,26 @@ describe('PatientSearchController', function () {
 
   var $componentController, $q, $compile, $rootScope, $state, ctrl, openmrsPatientMapper, patientService;
 
+  beforeEach(module('barcodeListener', function ($provide) {
+
+    // Prevent the real barcodeListener to be linked, otherwise it will complain about prefix not being a string.
+    // This is because AngularJS creates a different scope from the one provided to this directive's linking function.
+    $provide.factory('barcodeListenerDirective', function () {
+      return {
+        restrict: 'EA',
+        scope: {
+          onScan: '=',
+          prefix: '@',
+          length: '@',
+          scanDuration: '@?'
+        },
+        link: function() {
+        }
+      };
+    });
+
+  }));
+
   beforeEach(module('common.patient'));
 
   beforeEach(inject(function (_$componentController_, _$q_, _$compile_, _$rootScope_, _$state_, _openmrsPatientMapper_, _patientService_) {
@@ -48,32 +68,32 @@ describe('PatientSearchController', function () {
 
     var testPatient = {
       uuid: '11111111/11/11111',
-    }
+    };
 
     beforeEach(function () {
       spyOn(patientService, 'search').and.callFake(function () {
         return {
           then: (fn) => {
-            fn([testPatient])
+            fn([testPatient]);
             return {
               error: (errorFn) => errorFn(),
             }
           },
         }
-      })
+      });
 
       spyOn(openmrsPatientMapper, 'map').and.callFake(function () {
         return testPatient
-      })
-      
-      spyOn(ctrl, 'onPatientSelect')
+      });
+
+      spyOn(ctrl, 'onPatientSelect');
     });
 
     it("should automatically select patient after search", function () {
 
       // Add the bard code listener element with auto select set to true
-      const autoSelect = 'true'
-      var html = `<barcode-listener data-auto-select="${autoSelect}"><barcode-listener>`;
+      const autoSelect = 'true';
+      var html = `<barcode-listener data-auto-select="${autoSelect}" prefix=""><barcode-listener>`;
       const element = $compile(html)($rootScope);
       angular.element(document.body).append(element);
       $rootScope.$digest();
@@ -83,15 +103,15 @@ describe('PatientSearchController', function () {
       $rootScope.$apply();
       expect(patientService.search).toHaveBeenCalled();
       expect(openmrsPatientMapper.map).toHaveBeenCalled();
-      
-      expect(ctrl.onPatientSelect).toHaveBeenCalled()
+
+      expect(ctrl.onPatientSelect).toHaveBeenCalled();
     });
 
     it("should not automatically select patient after search", function () {
 
       // Add the bard code listener element with auto select set to false
-      const autoSelect = 'false'
-      var html = `<barcode-listener data-auto-select="${autoSelect}"><barcode-listener>`;
+      const autoSelect = 'false';
+      var html = `<barcode-listener data-auto-select="${autoSelect}" prefix=""><barcode-listener>`;
       const element = $compile(html)($rootScope);
       angular.element(document.body).append(element);
       $rootScope.$digest();
@@ -101,8 +121,8 @@ describe('PatientSearchController', function () {
       $rootScope.$apply();
       expect(patientService.search).toHaveBeenCalled();
       expect(openmrsPatientMapper.map).toHaveBeenCalled();
-      
-      expect(ctrl.onPatientSelect).toHaveBeenCalled()
+
+      expect(ctrl.onPatientSelect).toHaveBeenCalled();
     });
   });
 
