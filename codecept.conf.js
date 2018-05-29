b3039f5f38e192b1ccaf61d64f08acfedf1a9a67
Ryan James <rcjames1004@gmail.com>
2018-05-28 23:39:17 -0700

    [E2E tests] Use config from grunt

---
+++ codecept.conf.js
@@ -0,0 +1,52 @@
+var gruntConfig = require('./grunt.conf')()
+
+var options = gruntConfig.connect.options
+var proxy = gruntConfig.connect.proxies[0]
+
+module.exports.config = {
+  tests: './test/e2eTests/**/*_test.js',
+  timeout: 10000,
+  output: './test/e2eTests/output',
+  helpers: {
+    Puppeteer: {
+      url: `http://${options.hostname}:${options.port}`,
+      driver: 'hosted',
+      browser: 'chrome',
+      rootElement: 'body',
+      show: true,
+    },
+    REST: {
+      endpoint: `http://${proxy.host}${proxy.context}/ws/rest/v1`,
+      defaultHeaders: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic YWRtaW46ZVNhdWRlMTIz'
+      },
+    },
+  },
+  hooks: [
+    './test/e2eTests/hooks.js',
+  ],
+  include: {
+    I: './test/e2eTests/steps_file.js',
+
+    Data: './test/e2eTests/data.js',
+
+    DashboardPage: './test/e2eTests/pages/dashboardPage.js',
+    LoginPage: './test/e2eTests/pages/loginPage.js',
+    RegistrationDashPage: './test/e2eTests/pages/registrationDashboardPage.js',
+    RegistrationPage: './test/e2eTests/pages/registrationPage.js',
+
+    Apis: './test/e2eTests/rest/openMrsApis.js',
+  },
+  bootstrap: false,
+  mocha: {
+    reporterOptions: {
+      reportDir: 'test/e2eTests/reports',
+      reportFilename: 'pocE2E',
+      inlineAssets: true,
+      reportPageTitle: 'E2E Test Reports',
+      reportTitle: 'E2E Test Reports',
+    },
+  },
+  name: 'esaude-emr-poc',
+}
