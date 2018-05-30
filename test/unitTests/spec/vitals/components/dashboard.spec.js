f9c7f48beb55b65e98576e0dee32803f5c411fd3
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2018-05-30 11:42:01 +0200

    Coverage.

--- test/unitTests/spec/vitals/components/dashboard.spec.js
+++ test/unitTests/spec/vitals/components/dashboard.spec.js
@@ -1,51 +1,13 @@
 describe('dashboard', function () {
 
-  var $componentController, $state, $stateParams, patientService, visitService, $rootScope;
+  var $componentController, $state;
 
-  beforeEach(module('vitals', function ($provide, $translateProvider, $urlRouterProvider) {
-    // Mock translate asynchronous loader
-    $provide.factory('mergeLocaleFilesService', function ($q) {
-      return function () {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-      };
-    });
-    $translateProvider.useLoader('mergeLocaleFilesService');
-    $urlRouterProvider.deferIntercept();
-  }));
+  beforeEach(module('vitals'));
 
   beforeEach(inject(function (_$componentController_, _$q_, _$rootScope_, _patientService_, _visitService_, _$state_) {
     $componentController = _$componentController_;
-    $q = _$q_;
-    $rootScope = _$rootScope_;
-    patientService = _patientService_;
-    visitService = _visitService_;
     $state = _$state_;
   }));
-
-  describe('activate', function () {
-
-    var ctrl;
-
-    beforeEach(function () {
-      spyOn(patientService, 'getPatient').and.callFake(function () {
-        return $q(function (resolve) {
-          return resolve({});
-        });
-      });
-
-      spyOn(visitService, 'getTodaysVisit').and.callFake(function () {
-        return $q(function (resolve) {
-          return resolve({});
-        });
-      });
-
-      ctrl = $componentController('dashboard');
-
-    });
-
-  });
 
   describe('reload', function () {
 
