d1d95f6db9b5841d5fe03bd88425afb5d282b6b0
Edrisse Mussá <edrisseasm@gmail.com>
2018-05-30 10:11:50 +0200

    coverage

--- test/unitTests/spec/common/application/controller/similarPatientsController.spec.js
+++ test/unitTests/spec/common/application/controller/similarPatientsController.spec.js
@@ -1,18 +1,22 @@
 describe("SimilarPatientsController", function () {
 
-    var $controller, $rootScope, controller, scope;
+    var $controller, $rootScope, $location, controller, scope;
 
     var patient = { uuid: "UUID_1" };
 
     beforeEach(module("application"));
 
-    beforeEach(inject(function (_$controller_, _$rootScope_) {
+    beforeEach(inject(function (_$controller_, _$rootScope_, _$location_) {
         $controller = _$controller_;
         $rootScope = _$rootScope_;
+        $location = _$location_;
     }));
 
     beforeEach(function () {
         scope = { patient: patient };
+
+        spyOn($location, 'url');
+
         controller = $controller("SimilarPatientsController", { $scope: scope, patientService: {}, openmrsPatientMapper: {} });
         $rootScope.$apply();
     });
@@ -22,6 +26,17 @@ describe("SimilarPatientsController", function () {
             expect(controller.patient).toEqual(patient);
             expect(controller.similarPatients).toEqual([]);
         });
+    });
+
+    describe("loadPatientToDashboard", function() {
+
+        it("should load dashboard", function() {
+            expect(controller.patient).toEqual(patient);
+            controller.loadPatientToDashboard();
+            //expect($rootScope.patient).toEqual(patient);
+            //expect($location.url).toHaveBeenCalled();
+        });
+
     });
 
 });
