2b840365c15b7ae9701f2c3963b8ffeb1be5ad30
Yassin <yassin791@gmail.com>
2018-06-13 10:08:50 +0200

    Cancel check-in (#540)
    * Cancel check-in (Fixes #521)
    
    * Change checkIn binding name
    
    * Fix delete visit spec
    
    * Remove vm.disableCheckIn and improve error message

--- test/unitTests/spec/visit/components/checkIn.spec.js
+++ test/unitTests/spec/visit/components/checkIn.spec.js
@@ -18,7 +18,7 @@ describe('CheckInController', function () {
 
   describe('$onChanges', function () {
 
-    it('should disable check-in when there is a visit for today', function () {
+    it('should load visit for today', function () {
       var visit = { patient: { uuid: '7401f469-60ee-4cfa-afab-c1e89e2944e4' } };
       spyOn(visitService, 'getTodaysVisit').and.callFake(function () {
         return $q(function (resolve) {
@@ -29,20 +29,6 @@ describe('CheckInController', function () {
       ctrl.$onChanges({ patient: { currentValue: { uuid: '7401f469-60ee-4cfa-afab-c1e89e2944e4' } } });
       $rootScope.$apply();
       expect(ctrl.todayVisit).toEqual(visit);
-      expect(ctrl.disableCheckin).toEqual(true);
-    });
-
-    it('should check-in enabled when there is not visit for today', function () {
-      spyOn(visitService, 'getTodaysVisit').and.callFake(function () {
-        return $q(function (resolve) {
-          return resolve(null);
-        });
-      });
-      var ctrl = $componentController('checkIn');
-      ctrl.$onChanges({ patient: { currentValue: { uuid: '7401f469-60ee-4cfa-afab-c1e89e2944e4' } } });
-      $rootScope.$apply();
-      expect(ctrl.todayVisit).toBeUndefined();
-      expect(ctrl.disableCheckin).toBeUndefined();
     });
   });
 
@@ -77,7 +63,7 @@ describe('CheckInController', function () {
       });
 
       var ctrl = $componentController('checkIn', null, {
-        onCheckIn: function () {
+        onCheckInChange: function () {
         }
       });
 
@@ -88,44 +74,85 @@ describe('CheckInController', function () {
 
     });
 
-    it('should disable check-in', function () {
+    it('should call onCheckInChange binding', function () {
 
       var visit = {};
 
       spyOn(visitService, 'checkInPatient').and.callFake(function () {
+        return $q(function (resolve) {
+          return resolve(visit);
+        });
+      });
+
+      var ctrl = $componentController('checkIn', null, {onCheckInChange: jasmine.createSpy('onCheckInChange')});
+
+      ctrl.checkIn();
+      $rootScope.$apply();
+
+      expect(ctrl.onCheckInChange).toHaveBeenCalled();
+
+    });
+
+  });
+
+  describe('cancelCheckIn', function () {
+
+    it('should check-in the patient', function () {
+
+      var visit = {};
+
+      spyOn(visitService, 'deleteVisit').and.callFake(function () {
+        return $q(function (resolve) {
+          return resolve(visit);
+        });
+      });
+
+      var ctrl = $componentController('checkIn');
+
+      ctrl.cancelCheckIn();
+
+      expect(visitService.deleteVisit).toHaveBeenCalled();
+
+    });
+
+    it('should update the day\'s visit', function () {
+
+      var visit = {};
+
+      spyOn(visitService, 'deleteVisit').and.callFake(function () {
         return $q(function (resolve) {
           return resolve(visit);
         });
       });
 
       var ctrl = $componentController('checkIn', null, {
-        onCheckIn: function () {
+        onCheckInChange: function () {
         }
       });
 
-      ctrl.checkIn();
+      ctrl.cancelCheckIn();
       $rootScope.$apply();
 
-      expect(ctrl.disableCheckin).toEqual(true);
+      expect(ctrl.todayVisit).toEqual(null);
 
     });
 
-    it('should call onCheckIn binding', function () {
+    it('should call onCheckInChange binding', function () {
 
       var visit = {};
 
-      spyOn(visitService, 'checkInPatient').and.callFake(function () {
+      spyOn(visitService, 'deleteVisit').and.callFake(function () {
         return $q(function (resolve) {
           return resolve(visit);
         });
       });
 
-      var ctrl = $componentController('checkIn', null, { onCheckIn: jasmine.createSpy('onCheckIn') });
+      var ctrl = $componentController('checkIn', null, {onCheckInChange: jasmine.createSpy('onCheckInChange')});
 
-      ctrl.checkIn();
+      ctrl.cancelCheckIn();
       $rootScope.$apply();
 
-      expect(ctrl.onCheckIn).toHaveBeenCalled();
+      expect(ctrl.onCheckInChange).toHaveBeenCalled();
 
     });
 
