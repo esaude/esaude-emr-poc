bb7b73bc66895f34987822ce4e89d02256b88193
Sacur Ibraimo <sacuribraimo@gmail.com>
2018-06-18 12:25:43 +0200

    Fix conflicts

--- test/e2eTests/data.js
+++ test/e2eTests/data.js
@@ -1,30 +1,17 @@
 // Generates a patient identifier
 const generatePatientIdentifier = () => {
   // Generates a random number between [0,9]
-<<<<<<< HEAD
-  const g = () => Math.floor(Math.random() * Math.floor(9))
-
-  // Returns a randomly generated identifier of the format
-  // "PPDDUUSS/AA/NNNNN" where each is a number between [0, 9]
-  return `${g()}${g()}${g()}${g()}${g()}${g()}${g()}${g()}/${g()}${g()}/${g()}${g()}${g()}${g()}${g()}`
-}
-=======
   const g = () => Math.floor(Math.random() * Math.floor(9));
 
   // Returns a randomly generated identifier of the format
   // "PPDDUUSS/AA/NNNNN" where each is a number between [0, 9]
   return `${g()}${g()}${g()}${g()}${g()}${g()}${g()}${g()}/${g()}${g()}/${g()}${g()}${g()}${g()}${g()}`;
 };
->>>>>>> 3005ffb199db33258b1ed87ee0525fad04173bde
 
 // Defines the uuids of roles
 const Roles = {
   provider: '8d94f280-c2cc-11de-8d13-0010c6dffd0f',
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 3005ffb199db33258b1ed87ee0525fad04173bde
 
 // Data useful for tests
 module.exports = {
@@ -75,7 +62,6 @@ module.exports = {
 				Roles.provider,
 			],*/
     },
-<<<<<<< HEAD
   },
 
   // Utilities for generating provider related data
@@ -145,86 +131,11 @@ module.exports = {
     },
 
     patient3: {
-=======
-
-    patient1: {
-      "identifiers": [
-        {
-          "identifier": "30532063/03/33743",
-          "identifierType": "e2b966d0-1d5f-11e0-b929-000c29ad1d07", // NID (SERVICO TARV)
-          "location": "4c34a53f-b0c2-4315-9829-1a07f76e10a8", // Zumba
-          "preferred": true
-        }
-      ],
-      person: {
-        names: [
-          {
-            givenName: "Patient1FirstName",
-            familyName: "Patient1LastName"
-          }
-        ],
-        gender: 'M',
-        birthdate: (new Date('2002-10-2')).toISOString(),
-      },
-    },
-
-    patient2: {
-      "identifiers": [
-        {
-          "identifier": "88654738/73/84441",
-          "identifierType": "e2b966d0-1d5f-11e0-b929-000c29ad1d07", // NID (SERVICO TARV)
-          "location": "4c34a53f-b0c2-4315-9829-1a07f76e10a8", // Zumba
-          "preferred": true
-        }
-      ],
-      person: {
-        names: [
-          {
-            givenName: "Patient2FirstName",
-            familyName: "Patient2LastName"
-          }
-        ],
-        gender: 'F',
-        birthdate: (new Date('1994-3-19')).toISOString(),
-      },
-    },
-  },
-
-  // Utilities for generating provider related data
-  providers: {
-    // Generates JSON that can be used to make
-    // the user a provider
-    generateJsonFromUser: function (user) {
-      return {
-        person: user.person.uuid,
-        identifier: user.systemId
-      };
-    },
-  },
-
-  // Uuids for programs
-  programs: {
-    SERVICO_TARV_CUIDADO: '7b2e4a0a-d4eb-4df7-be30-78ca4b28ca99',
-    SERVICO_TARV_TRATAMENTO: 'efe2481f-9e75-4515-8d5a-86bfde2b5ad3',
-    TUBERCULOSE: '142d23c4-c29f-4799-8047-eb3af911fd21',
-    CCR: '611f0a6b-68b7-4de7-bc7a-fd021330eef8',
-    CCU: '8954a750-079e-4bf2-940c-b4f71ea8bb15',
-    PTV_ETV: '06057245-ca21-43ab-a02f-e861d7e54593',
-    CLINICA_MOVEL: 'fb455824-fb53-45ab-bf5a-a81482ff6848',
-  },
-
-  patients: {
-    patient1: {
->>>>>>> 3005ffb199db33258b1ed87ee0525fad04173bde
       "identifiers": [
         {
           "identifier1": "12345678/22/987654",
           "identifier2": "452631256M",
-<<<<<<< HEAD
           "identifier3": "12345678/22/98789",
-=======
-          "identifier3": "12345678/22/98799",
->>>>>>> 3005ffb199db33258b1ed87ee0525fad04173bde
           "alternativeIdentifier1": "12345",
           "alternativeIdentifier2": "234567",
           "preferred": true
@@ -260,8 +171,4 @@ module.exports = {
       }
     },
   }
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 3005ffb199db33258b1ed87ee0525fad04173bde
