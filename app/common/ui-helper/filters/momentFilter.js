0484ca90b648d4889c1214d718f1741ed292b43f
Edrisse Mussá <edrisseasm@gmail.com>
2018-06-27 11:36:52 +0200

    PR Review changes

--- app/common/ui-helper/filters/momentFilter.js
+++ app/common/ui-helper/filters/momentFilter.js
@@ -10,22 +10,17 @@
 
     angular
         .module("bahmni.common.uiHelper")
-        .filter("moment", () => filter);
-
-    function filter(input, format) {
-        var momentInput = input;
-        if (typeof input === 'string') {
-            momentInput = moment(input);
-        }
-        if (moment.isMoment(momentInput)) {
-            var customFormat = customFormats[format];
-            if (customFormat) {
-                format = customFormat;
-            } else if (!format) {
-                format = 'DD/MM/YYYY';
+        .filter("moment", (momentFormat) => {
+            return (input, format) => {
+                var momentInput = input;
+                if (angular.isString(input)) {
+                    momentInput = moment(input);
+                }
+                if (moment.isMoment(momentInput)) {
+                    var customFormat = momentFormat[format];
+                    return momentInput.format(customFormat);
+                }
+                throw OBJECT_IS_NOT_A_MOMENT + input;
             }
-            return momentInput.format(format);
-        }
-        throw OBJECT_IS_NOT_A_MOMENT + input;
-    }
+        });
 })();
